13166a8341857abde41e23e913e1ded7496989fe
Matt Frutchey <mjfru201@gmail.com>
2024-05-11 08:37:59 -0400

    add background notes about React, no code!

---
+++ react_notes/01react_basics/01react_intro.jsx
@@ -0,0 +1,30 @@
+/*
+! What Exactly is React?
+* A JavaScript library for building user interfaces.
+It is extremely popular, declarative, component-based, state-driven, JS library for building interfaces, created by Meta/Facebook.
+
+? Components
+* Components are the building blocks of user interfaces in React.
+* Components, like Lego pieces, come together to create a complex UI.
+
+? Declarative
+* We describe how components look and how they work using a declarative syntax called JSX.
+* JSX is syntax that combines HTML, CSS, JS, and references other components.
+* We tell React what a component should look like based on current data/state.
+* React is abstraction away from the DOM, we never actually touch the DOM!
+
+? State-driven
+* React reacts (and is called as such) to state changes by re-rendering the UI.
+
+? Framework? Library
+* Technically a library; it's just the 'view' layer...we need to pick multiple external libraries to build a complete application (MongoDB, Express, Node.js, etc.)
+* Complete frameworks have been built like NEXT.js & Remix on top of React.
+
+? Extremely Popular
+* By far the most used library/framework.
+* Many large companies have adopted React and therefore, smaller companies have followed in their footsteps.
+
+? Created by Facebook (Jordan Walke)
+* First used on the newsfeed and the chat-app.
+* Open-sourced in 2013 and has completely transformed front-end development.
+*/
