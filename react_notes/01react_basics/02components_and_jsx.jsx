ad692aed1c2e140bb95713e75a25fc2879a2c4cf
Matt Frutchey <mjfru201@gmail.com>
2024-05-15 00:50:45 -0400

    begin review notes on components and jsx usage

---
+++ react_notes/01react_basics/02components_and_jsx.jsx
@@ -0,0 +1,10 @@
+/*
+! Components
+* Components are the most fundamental aspect of React; applications are made entirely out of them!
+There is nothing that's not a component inside of a React app.
+* React takes components and 'draws' them onto a user interface, all these views it renders becomes what the user sees.
+* Each has its own data, logic, and appearance.
+! We build complex UIs by building multiple compnents and combining them together, like Lego pieces.
+* Components can be reused, nested inside each other, and pass data between them.
+Component trees are diagrams to visualize how they are linked to each other, parent/child relationships, etc.
+*/
