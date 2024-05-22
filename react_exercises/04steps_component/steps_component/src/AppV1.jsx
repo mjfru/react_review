6f8b04196f1933c9bd02722e3a50cd8528e74d71
Matt Frutchey <mjfru201@gmail.com>
2024-05-22 09:21:35 -0400

    work with children props to increase usability of buttons

--- react_exercises/04steps_component/steps_component/src/AppV1.jsx
+++ react_exercises/04steps_component/steps_component/src/AppV1.jsx
@@ -42,29 +42,18 @@ function App() {
           <p className="message">Step {step}: {messages[step - 1]}</p>
           
           <div className="buttons">
-            <Button 
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}><span>⏪</span>Previous</Button>
-            
-            <Button 
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handleNext}>Next <span>⏩</span> </Button>
+            <button style={{backgroundColor: "#7950f2", color: '#fff'}} 
+              onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={{backgroundColor: "#7950f2", color: '#fff'}} onClick={handleNext}>
+              Next
+            </button>
           </div>
         </div>
         )
       }
     </>
-  )
-}
-
-//* Children props are pre-defined inside React and refer to anything coming inbetween the <Button></Button> component tags.
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-  <button 
-    style={{backgroundColor: bgColor, color: textColor}} 
-    onClick={onClick}>{children}</button>
   )
 }
 
