fcd6524edcb48066d4b952d6ec9d2773a45c9e04
Matt Frutchey <mjfru201@gmail.com>
2024-05-22 09:23:24 -0400

    complete thinking in react notes

---
+++ react_notes/01react_basics/04thinking_react.jsx
@@ -0,0 +1,31 @@
+/*
+! Thinking in React
+* Thinking in React is a Core Skill -- It's different from building applications in other ways!
+
+What does this mean?
+* You can recognize situations where to use appropriate hooks, components, data flow, etc.
+* You think in state transitions, not element mutations.
+
+? The Thinking in React Process:
+
+1. Break the desired UI into components and establish the component tree.
+2. Build a static version in React (without state!)
+3. Think about state
+  - When to use it
+  - Types of state: local / global
+  - Where to place each piece of state
+
+4. Establish data flow:
+  - One-way data flow
+  - Child-to-parent communication
+  - Accessing global state
+
+* When you've mastered these, you'll be able to answer questions like:
+  - How to break up a UI into components?
+  - How to make some components reusable?
+  - How to assemble UI from reusable components?
+  - What pieces of state do I need for interactivity?
+  - Where to place state?
+  - What types of state can or should I use?
+  - How do I make data flow through the application?
+*/
