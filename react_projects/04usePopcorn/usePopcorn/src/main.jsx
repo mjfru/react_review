8f461227c528ea2146452d01734d202c8ca8bd2d
Matt Frutchey <mjfru201@gmail.com>
2024-06-06 09:17:21 -0400

    add more tests and use cases

--- react_projects/04usePopcorn/usePopcorn/src/main.jsx
+++ react_projects/04usePopcorn/usePopcorn/src/main.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import App from './App.jsx'
 // import './index.css'
 
-import StarRating from './components/utility_components/StarRating'
+import StarRating from "./components/utility_components/StarRating";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Example where we need access to the state.
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was rated {movieRating} stars.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-  </React.StrictMode>,
-)
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
+  </React.StrictMode>
+);
