120dced94608eade34278612ef32e3d302c6c9f5
Matt Frutchey <mjfru201@gmail.com>
2024-06-09 08:48:31 -0400

    make a practice App-test.jsx to experiment with right and wrong ways to fetch data in react

---
+++ react_projects/04usePopcorn/usePopcorn/src/App-test.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import Navbar from "./components/navbar_components/Navbar";
+import Search from "./components/navbar_components/Search";
+import NumResults from "./components/navbar_components/NumResults";
+import Main from "./components/main_components/Main";
+import Box from "./components/main_components/Box";
+import Summary from "./components/main_components/Summary";
+import MovieList from "./components/main_components/MovieList";
+import WatchedList from "./components/main_components/WatchedList";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const KEY = "799bee64";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+
+  const query = "interstellar"
+  //* Good practice, using useEffect to do something as soon as an application loads.
+  useEffect(function() {
+    async function fetchMovies() {
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`)
+      const data = await res.json()
+      setMovies(data.Search);
+      console.log(data.Search);
+    }
+    fetchMovies();
+  }, []);
+
+  //! Bad practice, this will continously re-render, an infinite loop of state-setting
+  // fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data.Search))
+
+  // setWatched([]);
+
+  const average = (arr) =>
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+  return (
+    <>
+      {/* Component Composition~!~! Navbar receiving a children prop, Main receiving two, and Listbox receiving one within Main. */}
+      <Navbar>
+        <Search />
+        <NumResults movies={movies} />
+      </Navbar>
+      <Main>
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+
+        {/* We can also do something like this, which is referred to as passing elements as props 
+      <Main>
+        <Box element={<MovieList movies={movies} />} /> 
+            Box.jsx receives element as a prop.
+      This kind of pattern is used with ReactRouter
+      */}
+
+        <Box>
+          <Summary watched={watched} average={average} />
+          <WatchedList watched={watched} />
+        </Box>
+      </Main>
+    </>
+  );
+}
+
+export default App;
--- react_projects/04usePopcorn/usePopcorn/src/main.jsx
+++ react_projects/04usePopcorn/usePopcorn/src/main.jsx
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-// import './index.css'
+import App from './App-test.jsx'
+import './index.css'
 
 import StarRating from "./components/utility_components/StarRating";
 
 // Example where we need access to the state.
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movies was rated {movieRating} stars.</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movies was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
