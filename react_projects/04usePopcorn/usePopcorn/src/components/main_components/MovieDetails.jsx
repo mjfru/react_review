e54ab5e5511b791d71d4124ddd17832b238e871b
Matt Frutchey <mjfru201@gmail.com>
2024-06-10 09:29:19 -0400

    add error message, loader, and movie details components

---
+++ react_projects/04usePopcorn/usePopcorn/src/components/main_components/MovieDetails.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import StarRating from "../utility_components/StarRating";
+import Loader from "./Loader";
+
+function MovieDetails({ selectedId, onCloseMovie, KEY }) {
+  // We get back an object from our API call, so we set the initial state to an empty object.
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  console.log(title, year);
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false);
+      }
+      getMovieDetails();
+      // Remount whenever the selectedId changes
+    },
+    [selectedId]
+  );
+
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <button className="btn-back" onClick={onCloseMovie}>
+              &larr;
+            </button>
+            <img src={poster} alt={`Poster of ${title} movie.`} />
+            <div className="details-overview">
+              <h2>{title}</h2>
+              <p>
+                {released} &bull; {runtime}
+              </p>
+              <p>{genre}</p>
+              <p>
+                <span>⭐</span>
+                {imdbRating} IMDb Rating
+              </p>
+            </div>
+          </header>
+          <section>
+            <div className="rating">
+              <StarRating maxRating={10} size={24} />
+            </div>
+            <p>
+              <em>{plot}</em>
+            </p>
+            <p>Starring: {actors}</p>
+            <p>Directed by: {director}</p>
+          </section>
+        </>
+      )}
+      {selectedId}
+    </div>
+  );
+}
+
+export default MovieDetails;
