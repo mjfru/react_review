e317eb2abafe7c033f0b5f20b00a4d6d8ef30ac3
Matt Frutchey <mjfru201@gmail.com>
2024-06-11 00:28:19 -0400

    add directory for secondary, faster-paced course

---
+++ react_crash_course/rcc_notes/01about_react.jsx
@@ -0,0 +1,9 @@
+/*
+! What is React & What Problem Does It Solve?
+* React is a JS library for building user interfaces!
+It's popular and a widely-sought after skill...but why??
+
+* React is able to only change the small, changed pieces of the DOM instead of re-rendering the entire page.
+* We only worry about the data in our app and 'reacts' to user input and changes in our UI.
+
+*/
