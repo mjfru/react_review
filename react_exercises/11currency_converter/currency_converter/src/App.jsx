213995b4998dcacc8d3d3f9ff82f4c5528164834
Matt Frutchey <mjfru201@gmail.com>
2024-06-13 06:54:56 -0400

    finish exercise, using useEffect as the main topic practiced

---
+++ react_exercises/11currency_converter/currency_converter/src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function convert() {
+      setIsLoading(true);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      setConverted(data.rates[toCurrency]);
+      setIsLoading(false);
+    }
+
+    if (fromCurrency === toCurrency) return setConverted(amount);
+    convert();
+  }, [amount, toCurrency, fromCurrency]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{converted} {toCurrency}</p>
+    </div>
+  );
+}
+
+export default App;
