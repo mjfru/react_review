aaccdd791046c419eb957206d1f2be58735e79d9
Matt Frutchey <mjfru201@gmail.com>
2024-06-16 08:54:12 -0400

    finish react quiz, adding full functionality: scores, progress, reset, etc.

--- react_projects/07react_quiz/react_quiz/src/App.jsx
+++ react_projects/07react_quiz/react_quiz/src/App.jsx
@@ -3,18 +3,29 @@ import Body from "./components/Body";
 import Loader from "./components/Loader";
 import Error from "./components/Error";
 import Questions from "./components/Questions";
+import NextButton from "./components/NextButton";
 import "./App.css";
 import { useEffect, useReducer } from "react";
 import StartScreen from "./components/StartScreen";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+import Footer from "./components/Footer";
+import Timer from "./components/Timer";
 
 //* Initial state for useReducer to use
 const initialState = {
   questions: [],
-  // Status will change between: 'loading', 'error', 'ready', 'active', 'finished' instead of just a setStatus / status state.
+  // Status will change between: 'loading', 'error', 'ready', 'active', 'finished', etc instead of just a setStatus / status state.
   status: "loading",
   // Keeping track of which question is the current one, starts at 0, the first one.
   index: 0,
+  answer: null,
+  points: 0,
+  highScore: 0,
+  secondsRemaining: null,
 };
+
+const SEC_PER_QUESTION = 20;
 
 //* Reducer function for useReducer, taking in the state and the action that was dispatched
 function reducer(state, action) {
@@ -25,13 +36,55 @@ function reducer(state, action) {
         questions: action.payload,
         status: "ready",
       };
+
     case "dataFailed":
       return {
         ...state,
         status: "error",
       };
+
     case "start":
-      return { ...state, status: "active" };
+      return { ...state, status: "active", secondsRemaining: state.questions.length * SEC_PER_QUESTION };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+
+    case "reset":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -39,10 +92,17 @@ function reducer(state, action) {
 
 function App() {
   //* We're going to want to display our fetched data on the UI and need state, a perfect chance to use useReducer to create it.
-  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+  const [
+    { questions, status, index, answer, points, highScore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   //* Derived state:
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   //* We want to use our questions data (set up on a JSON server, also located in the data folder here), so let's use useEffect when fetching it.
   useEffect(function () {
@@ -63,7 +123,39 @@ function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Questions question={questions[index]}/>}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Questions
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highScore={highScore}
+            dispatch={dispatch}
+          />
+        )}
       </Body>
     </div>
   );
---
+++ react_projects/07react_quiz/react_quiz/src/components/FinishScreen.jsx
@@ -0,0 +1,27 @@
+export default function FinishScreen({ points, maxPossiblePoints, highScore, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if(percentage === 100) emoji = '💯';
+  if(percentage >= 80 && percentage < 100) emoji = '👏';
+  if(percentage >= 50 && percentage < 80) emoji = '👍';
+  if(percentage >= 0 && percentage < 50) emoji = '🤔';
+  if(percentage === 0) emoji = '🤦‍♂️';
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">Highscore: {highScore}</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "reset" })}
+      >
+        Restart Quiz
+        </button>
+    </>
+  );
+}
