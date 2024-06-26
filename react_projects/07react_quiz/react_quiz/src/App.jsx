769327843589eb97590d72fb98c5ee54af396ee6
Matt Frutchey <mjfru201@gmail.com>
2024-06-26 09:02:09 -0400

    integrate useContext and a context file into project

--- react_projects/07react_quiz/react_quiz/src/App.jsx
+++ react_projects/07react_quiz/react_quiz/src/App.jsx
@@ -1,166 +1,39 @@
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Loader from "./components/Loader";
-import Error from "./components/Error";
-import Questions from "./components/Questions";
-import NextButton from "./components/NextButton";
-import "./App.css";
-import { useEffect, useReducer } from "react";
-import StartScreen from "./components/StartScreen";
-import Progress from "./components/Progress";
-import FinishScreen from "./components/FinishScreen";
-import Footer from "./components/Footer";
-import Timer from "./components/Timer";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+import { useQuiz } from "../contexts/QuizContext";
 
-//* Initial state for useReducer to use
-const initialState = {
-  questions: [],
-  // Status will change between: 'loading', 'error', 'ready', 'active', 'finished', etc instead of just a setStatus / status state.
-  status: "loading",
-  // Keeping track of which question is the current one, starts at 0, the first one.
-  index: 0,
-  answer: null,
-  points: 0,
-  highScore: 0,
-  secondsRemaining: null,
-};
-
-const SEC_PER_QUESTION = 20;
-
-//* Reducer function for useReducer, taking in the state and the action that was dispatched
-function reducer(state, action) {
-  switch (action.type) {
-    case "dataReceived":
-      return {
-        ...state,
-        questions: action.payload,
-        status: "ready",
-      };
-
-    case "dataFailed":
-      return {
-        ...state,
-        status: "error",
-      };
-
-    case "start":
-      return { ...state, status: "active", secondsRemaining: state.questions.length * SEC_PER_QUESTION };
-
-    case "newAnswer":
-      const question = state.questions.at(state.index);
-      return {
-        ...state,
-        answer: action.payload,
-        points:
-          action.payload === question.correctOption
-            ? state.points + question.points
-            : state.points,
-      };
-
-    case "nextQuestion":
-      return {
-        ...state,
-        index: state.index + 1,
-        answer: null,
-      };
-
-    case "finish":
-      return {
-        ...state,
-        status: "finished",
-        highScore:
-          state.points > state.highScore ? state.points : state.highScore,
-      };
-
-    case "reset":
-      return {
-        ...initialState,
-        questions: state.questions,
-        status: "ready",
-      };
-
-    case "tick":
-      return {
-        ...state,
-        secondsRemaining: state.secondsRemaining - 1,
-        status: state.secondsRemaining === 0 ? "finished" : state.status,
-      };
-    default:
-      throw new Error("Action unknown");
-  }
-}
-
-function App() {
-  //* We're going to want to display our fetched data on the UI and need state, a perfect chance to use useReducer to create it.
-  const [
-    { questions, status, index, answer, points, highScore, secondsRemaining },
-    dispatch,
-  ] = useReducer(reducer, initialState);
-
-  //* Derived state:
-  const numQuestions = questions.length;
-  const maxPossiblePoints = questions.reduce(
-    (prev, cur) => prev + cur.points,
-    0
-  );
-
-  //* We want to use our questions data (set up on a JSON server, also located in the data folder here), so let's use useEffect when fetching it.
-  useEffect(function () {
-    fetch("http://localhost:8000/questions")
-      .then((res) => res.json())
-      //* Creating an event which our reducer will respond to and the information (payload) it's providing:
-      .then((data) => dispatch({ type: "dataReceived", payload: data }))
-      .catch((err) => dispatch({ type: "dataFailed" }));
-  }, []);
+export default function App() {
+  const { status } = useQuiz();
 
   return (
     <div className="app">
       <Header />
-      <Body>
+
+      <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {/* Passing around 'dispatch' like we pass around event handler / setState functions. */}
-        {status === "ready" && (
-          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
-        )}
+        {status === "ready" && <StartScreen />}
         {status === "active" && (
           <>
-            <Progress
-              index={index}
-              numQuestions={numQuestions}
-              points={points}
-              maxPossiblePoints={maxPossiblePoints}
-              answer={answer}
-            />
-            <Questions
-              question={questions[index]}
-              dispatch={dispatch}
-              answer={answer}
-            />
+            <Progress />
+            <Question />
             <Footer>
-              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
-              <NextButton
-                dispatch={dispatch}
-                answer={answer}
-                index={index}
-                numQuestions={numQuestions}
-              />
+              <Timer />
+              <NextButton />
             </Footer>
           </>
         )}
-        {status === "finished" && (
-          <FinishScreen
-            points={points}
-            maxPossiblePoints={maxPossiblePoints}
-            highScore={highScore}
-            dispatch={dispatch}
-          />
-        )}
-      </Body>
+        {status === "finished" && <FinishScreen />}
+      </Main>
     </div>
   );
 }
-
-export default App;
-
-//! npm run server - Port 8000
