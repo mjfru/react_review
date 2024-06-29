48a15432c36230680c28787e2a873ffbb24e714b
Matt Frutchey <mjfru201@gmail.com>
2024-06-29 09:21:54 -0400

    add note on styling options w/ React

---
+++ react_notes/03react_advanced_&_redux/03styling_options.jsx
@@ -0,0 +1,13 @@
+/*
+! Styling Options in React
+* React does NOT care about how you style your applications.
+
+Styling Option:         Where?              How?                  Scope           Based On
+Inline CSS              JSX Elements          style prop            JSX element     CSS
+CSS/Sass file           External File         className prop        Entire App      CSS
+CSS Modules             One file / component  className prop        Component       CSS
+CSS-in-JS               External / Component  Creates new component Component       JS
+Utility CSS (Tailwind)  JSX Elements          className prop        JSX element     CSS
+
+* Alternatives to styling w/ CSS: UI libraries like MUI, Chakra, Mantine, etc.
+*/
