0bf15403ebdf37fc69c101d453492efde37f9f94
Matt Frutchey <mjfru201@gmail.com>
2024-07-02 10:27:11 -0400

    implement new utility components, style forms

--- react_projects/12fast_react_pizza/fast_react_pizza/src/features/order/SearchOrder.jsx
+++ react_projects/12fast_react_pizza/fast_react_pizza/src/features/order/SearchOrder.jsx
@@ -19,6 +19,7 @@ function SearchOrder() {
         placeholder="Search Order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 text-sm transition-all duration-300 bg-yellow-100 rounded-full placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50"
       />
     </form>
   );
--- react_projects/12fast_react_pizza/fast_react_pizza/src/ui/AppLayout.jsx
+++ react_projects/12fast_react_pizza/fast_react_pizza/src/ui/AppLayout.jsx
@@ -9,12 +9,14 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-auto">
+        <main className="max-w-3xl mx-auto">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
