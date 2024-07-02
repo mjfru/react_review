7b7daa9f435b5ac883c646c47c7d4704186e9463
Matt Frutchey <mjfru201@gmail.com>
2024-07-02 10:26:24 -0400

    add utility button component for forms

---
+++ react_projects/12fast_react_pizza/fast_react_pizza/src/ui/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Button({ children, disabled, to }) {
+  const className = "inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4"
+  
+  if(to) {
+    return <Link className={className} to={to}>{children}</Link>
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      className={className}
+    >
+      {children}
+    </button>
+  );
+}
