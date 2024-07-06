8925bc2be36ba41d64d52a76d0c12c3c46895a5f
Matt Frutchey <mjfru201@gmail.com>
2024-07-06 09:20:11 -0400

    Revert "complete project by adding geolocation service, bug fixing"
    This reverts commit 1a8f724ab11c26d9618656ea519ed190688a933d.
    "Push from wrong file, reverted"

--- react_crash_course/rcc_notes/02context.jsx
+++
@@ -1,5 +0,0 @@
-/*
-! Context - An Elegant Way to Pass / Share Data
-* Context is useful in situations where we are prop drilling too much or have a piece of state that everything or many things needs.
-* 
-*/
--- react_projects/12fast_react_pizza/fast_react_pizza/src/services/apiGeocoding.js
+++ react_projects/12fast_react_pizza/fast_react_pizza/src/services/apiGeocoding.js
@@ -1,6 +1,5 @@
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
-    // Basically reverse geo-coding
     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
   );
   if (!res.ok) throw Error("Failed getting address");
