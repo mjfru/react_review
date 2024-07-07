362b15306013649de88e86617a4221948a0ac643
Matt Frutchey <mjfru201@gmail.com>
2024-07-07 08:52:50 -0400

    implement example reducer function and useReducer hook

--- react_crash_course/rcc_projects/complex_react_app/app/Main.js
+++ react_crash_course/rcc_projects/complex_react_app/app/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from "axios";
@@ -16,6 +16,27 @@ import ViewSinglePost from "./components/ViewSinglePost";
 import ExampleContext from "./ExampleContext";
 
 function Main() {
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexAppToken")),
+    flashMessages: [],
+  };
+
+  function ourReducer(state, action) {
+    switch (action.type) {
+      case "login":
+        return { loggedIn: true, flashMessages: state.flashMessages };
+      case "logout":
+        return { loggedIn: false, flashMessages: state.flashMessages };
+      case "flashMessage":
+        return {
+          loggedIn: state.loggedIn,
+          flashMessages: state.flashMessages.concat(action.value),
+        };
+    }
+  }
+
+  const [state, dispatch] = useReducer(ourReducer, initialState);
+
   const [loggedIn, setLoggedIn] = useState(
     Boolean(localStorage.getItem("complexAppToken"))
   );
