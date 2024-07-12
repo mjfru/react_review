17775f89d7cf65da96e75f40e12ee92e4e90d299
Matt Frutchey <mjfru201@gmail.com>
2024-07-12 07:20:27 -0400

    complete feature to add a new cabin from the respective page

--- react_projects/13wild_oasis/the_wild_oasis/src/services/apiCabins.js
+++ react_projects/13wild_oasis/the_wild_oasis/src/services/apiCabins.js
@@ -9,6 +9,19 @@ export default async function getCabins() {
   return data;
 }
 
+// When this gets called from the form page, the data that's passed in matches the shape of cabin data exactly (k-v pairs).
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([ newCabin ])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
