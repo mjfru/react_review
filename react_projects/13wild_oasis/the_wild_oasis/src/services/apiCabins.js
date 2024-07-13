a3edd099bb7c07d02967a18d9931c867c390edfd
Matt Frutchey <mjfru201@gmail.com>
2024-07-13 08:59:06 -0400

    successfully integrate image upload with supabase

--- react_projects/13wild_oasis/the_wild_oasis/src/services/apiCabins.js
+++ react_projects/13wild_oasis/the_wild_oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export default async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -11,13 +11,34 @@ export default async function getCabins() {
 
 // When this gets called from the form page, the data that's passed in matches the shape of cabin data exactly (k-v pairs).
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create the cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([ newCabin ])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created");
+  }
+
+  // https://irpfjpoxgxdugrzbbibd.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg
+  //  2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete cabin if there was an error uploading corresponding image:
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error("Cabin image could not be uploaded -- Cabin not created.");
   }
   return data;
 }
--- react_projects/13wild_oasis/the_wild_oasis/src/services/supabase.js
+++ react_projects/13wild_oasis/the_wild_oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://irpfjpoxgxdugrzbbibd.supabase.co";
+export const supabaseUrl = "https://irpfjpoxgxdugrzbbibd.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlycGZqcG94Z3hkdWdyemJiaWJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA0NDgyNzUsImV4cCI6MjAzNjAyNDI3NX0.1wPYZ3Ol27lw5CcCIaEjwxqlkPpYmr7KS16kC4PSX4s";
 const supabase = createClient(supabaseUrl, supabaseKey);
