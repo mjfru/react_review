b99aecfba4ab4c48e161895fda169dd9e067ae40
Matt Frutchey <mjfru201@gmail.com>
2024-07-18 11:57:21 -0400

    add chat, post, profilefollowers/following components

---
+++ react_crash_course/rcc_projects/complex_react_app/app/components/ProfileFollowing.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import LoadingDotsIcon from "./LoadingDotsIcon";
+
+export default function ProfileFollowing() {
+  const { username } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const ourRequest = new AbortController();
+    async function fetchPosts() {
+      try {
+        const response = await axios.get(`/profile/${username}/following`);
+        setPosts(response.data);
+        setIsLoading(false);
+        // console.log(response.data);
+      } catch (e) {
+        console.log("An error has occurred." + e);
+      }
+    }
+    fetchPosts();
+    return () => {
+      ourRequest.abort();
+    };
+  }, [username]);
+
+  if (isLoading) return <LoadingDotsIcon />;
+
+  return (
+    <div className="list-group">
+      {posts.map((follower, index) => {
+        return (
+          <Link
+            key={index}
+            to={`/profile/${follower.username}`}
+            className="list-group-item list-group-item-action"
+          >
+            <img className="avatar-tiny" src={follower.avatar} />
+            {follower.username}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
