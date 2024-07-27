0cf18a25b1b9026e25ba983d756665dbcff70385
Matt Frutchey <mjfru201@gmail.com>
2024-07-27 09:18:55 -0400

    add the UI for sorting and filtering

--- react_projects/13wild_oasis/the_wild_oasis/src/ui/Filter.jsx
+++ react_projects/13wild_oasis/the_wild_oasis/src/ui/Filter.jsx
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { useSearchParams } from "react-router-dom";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +34,30 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+// Props being passed in so this can be reused on other pages-- what variables could be different based on what we're trying to filter
+export default function Filter({ filterField, options }) {
+  // Store the value of what filter buttons we click on in the URL to easily share
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          onClick={() => handleClick(option.value)}
+          key={option.value}
+          active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
