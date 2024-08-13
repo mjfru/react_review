0f6cf6a29dac8f14836f4ddc4818a0a29c4d6962
Matt Frutchey <mjfru201@gmail.com>
2024-08-13 00:38:11 -0400

    add stats feature for bookings, sales, checkings, and occupency rate

---
+++ react_projects/13wild_oasis/the_wild_oasis/src/features/dashboard/Stats.jsx
@@ -0,0 +1,58 @@
+import Stat from "./Stat";
+import {
+  HiOutlineBanknotes,
+  HiOutlineBriefcase,
+  HiOutlineCalendarDays,
+  HiOutlineChartBar,
+} from "react-icons/hi2";
+import { formatCurrency } from "../../utils/helpers";
+
+export default function Stats({
+  bookings,
+  confirmedStays,
+  numDays,
+  cabinCount,
+}) {
+  // 1. The number of bookings:
+  const numBookings = bookings.length;
+
+  // 2. Sales:
+  const sales = bookings.reduce((acc, cur) => acc + cur.totalPrice, 0);
+
+  // 3. Check-ins
+  const checkins = confirmedStays.length;
+
+  // 4. Occupancy rate
+  const occupancy =
+    confirmedStays.reduce((acc, cur) => acc + cur.numNights, 0) /
+    (numDays * cabinCount);
+
+  return (
+    <>
+      <Stat
+        title="Bookings"
+        color="blue"
+        icon={<HiOutlineBriefcase />}
+        value={numBookings}
+      />
+      <Stat
+        title="Sales"
+        color="green"
+        icon={<HiOutlineBanknotes />}
+        value={formatCurrency(sales)}
+      />
+      <Stat
+        title="Check ins"
+        color="indigo"
+        icon={<HiOutlineCalendarDays />}
+        value={checkins}
+      />
+      <Stat
+        title="Occupancy rate"
+        color="yellow"
+        icon={<HiOutlineChartBar />}
+        value={Math.round(occupancy * 100) + '%'}
+      />
+    </>
+  );
+}
