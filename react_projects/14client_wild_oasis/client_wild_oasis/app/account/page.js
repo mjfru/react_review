a71c054d2b5b573beaa1898999b3bad231c3cdb8
Matt Frutchey <mjfru201@gmail.com>
2024-08-22 09:41:03 -0400

    add all work so far on Next.js project, a client-facing web app

---
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guest Area",
+};
+
+export default function Page() {
+  return (
+    <h2 className="text-2xl font-semibold text-accent-400 mb-7">
+      Welcome, User!
+    </h2>
+  );
+}
