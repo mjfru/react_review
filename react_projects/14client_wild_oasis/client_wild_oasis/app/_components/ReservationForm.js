0edf026a6bb183744972a03ae53b4d544d341773
Matt Frutchey <mjfru201@gmail.com>
2024-08-31 09:27:08 -0400

    implement the res. form for logged in users

---
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/_components/ReservationForm.js
@@ -0,0 +1,71 @@
+"use client";
+
+import { useReservation } from "./ReservationContext";
+
+function ReservationForm({ cabin, user }) {
+	// CHANGE
+	const { range } = useReservation;
+	const { maxCapacity } = cabin;
+
+	return (
+		<div className="scale-[1.01]">
+			<div className="flex items-center justify-between px-16 py-2 bg-primary-800 text-primary-300">
+				<p>Logged in as</p>
+
+				<div className="flex items-center gap-4">
+					<img
+						// Important to display google profile images
+						referrerPolicy="no-referrer"
+						className="h-8 rounded-full"
+						src={user.image}
+						alt={user.name}
+					/>
+					<p>{user.name}</p>
+				</div>
+			</div>
+
+			<form className="flex flex-col gap-5 px-16 py-10 text-lg bg-primary-900">
+				<div className="space-y-2">
+					<label htmlFor="numGuests">How many guests?</label>
+					<select
+						name="numGuests"
+						id="numGuests"
+						className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800"
+						required
+					>
+						<option value="" key="">
+							Select number of guests...
+						</option>
+						{Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
+							<option value={x} key={x}>
+								{x} {x === 1 ? "guest" : "guests"}
+							</option>
+						))}
+					</select>
+				</div>
+
+				<div className="space-y-2">
+					<label htmlFor="observations">
+						Anything we should know about your stay?
+					</label>
+					<textarea
+						name="observations"
+						id="observations"
+						className="w-full px-5 py-3 rounded-sm shadow-sm bg-primary-200 text-primary-800"
+						placeholder="Any pets, allergies, special requirements, etc.?"
+					/>
+				</div>
+
+				<div className="flex items-center justify-end gap-6">
+					<p className="text-base text-primary-300">Start by selecting dates</p>
+
+					<button className="px-8 py-4 font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+						Reserve now
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default ReservationForm;
