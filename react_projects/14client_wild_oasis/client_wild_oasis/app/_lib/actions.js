cd51a960cf64482887a76a0807523d07cd83bd40
Matt Frutchey <mjfru201@gmail.com>
2024-09-20 09:02:15 -0400

    add revalidation to update the form if the user goes back to it

--- react_projects/14client_wild_oasis/client_wild_oasis/app/_lib/actions.js
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
 	const session = await auth();
@@ -49,19 +50,45 @@ export async function deleteReservation(bookingId) {
 }
 
 export async function updateBooking(formData) {
-	console.log(formData);
+	// Coming from our hidden input in the form
+	const bookingId = Number(formData.get("bookingId"));
 
-	// const { data, error } = await supabase
-	// 	.from("bookings")
-	// 	.update(updatedFields)
-	// 	.eq("id", id)
-	// 	.select()
-	// 	.single();
+	// 1. Authentication
+	const session = await auth();
+	if (!session) throw new Error("You must be logged in.");
 
-	// if (error) {
-	// 	console.error(error);
-	// 	throw new Error("Booking could not be updated");
-	// }
+	// 2. Authorization
+	const guestBookings = await getBookings(session.user.guestId);
+	const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+	if (!guestBookingIds.includes(bookingId))
+		throw new Error("You are not allowed to update this booking.");
+
+	// 3. Building the update data
+	const updateData = {
+		numGuests: Number(formData.get("numGuests")),
+		observations: formData.get("observations").slice(0, 1000),
+	};
+
+	// 4. Mutation
+	const { error } = await supabase
+		.from("bookings")
+		.update(updateData)
+		.eq("id", bookingId)
+		.select()
+		.single();
+
+	// 5. Error Handling
+	if (error) {
+		throw new Error("Booking could not be updated");
+	}
+
+	//6. Revalidation
+	revalidatePath("/account/reservations", 'layout');
+	// revalidatePath(`/account/reservations/${bookingId}`);
+
+	// 7. Redirecting
+	redirect("/account/reservations");
 }
 
 export async function signInAction() {
