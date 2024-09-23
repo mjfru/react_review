0716aee9dd232873639d8d3eb95a9b451b835d28
Matt Frutchey <mjfru201@gmail.com>
2024-09-23 06:56:57 -0400

    have the DeleteReservation component accept onDelete as a prop

--- react_projects/14client_wild_oasis/client_wild_oasis/app/_components/ReservationCard.js
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/_components/ReservationCard.js
@@ -9,7 +9,7 @@ export const formatDistanceFromNow = (dateStr) =>
 		addSuffix: true,
 	}).replace("about ", "");
 
-function ReservationCard({ booking }) {
+function ReservationCard({ booking, onDelete }) {
 	const {
 		id,
 		guestId,
@@ -81,7 +81,7 @@ function ReservationCard({ booking }) {
 							<PencilSquareIcon className="w-5 h-5 transition-colors text-primary-600 group-hover:text-primary-800" />
 							<span className="mt-1">Edit</span>
 						</Link>
-						<DeleteReservation bookingId={id} />
+						<DeleteReservation bookingId={id} onDelete={onDelete} />
 					</>
 				) : (
 					""
