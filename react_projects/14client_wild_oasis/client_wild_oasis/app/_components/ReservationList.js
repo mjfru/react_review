c93f6d9b48d5e2e3a503038fc64169f88744b457
Matt Frutchey <mjfru201@gmail.com>
2024-09-23 06:56:19 -0400

    move a handleDelete function, accept it as a prop, and set up the useOptimisitic hook

--- react_projects/14client_wild_oasis/client_wild_oasis/app/_components/ReservationList.js
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/_components/ReservationList.js
@@ -1,10 +1,28 @@
+"use client";
+import { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
 
 export default function ReservationList({ bookings }) {
+	// Think about the actual state and the 'optimistic' state when using this.
+	//* [optimisticState, setterFunction ] = useOptimistic(currentState, an update function)
+	const [optimisticBookings, optimisticDelete] = useOptimistic(
+		bookings,
+		() => {}
+	);
+
+	async function handleDelete(bookingId) {
+		await deleteReservation(bookingId);
+	}
+
 	return (
 		<ul className="space-y-6">
 			{bookings.map((booking) => (
-				<ReservationCard booking={booking} key={booking.id} />
+				<ReservationCard
+					booking={booking}
+					key={booking.id}
+					onDelete={handleDelete}
+				/>
 			))}
 		</ul>
 	);
