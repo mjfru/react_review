95b5b862f2337126359443490db0beb96f0dd65a
Matt Frutchey <mjfru201@gmail.com>
2024-09-24 09:37:12 -0400

    implement useTransition for using a loading icon when deleting a booking

--- react_projects/14client_wild_oasis/client_wild_oasis/app/_components/DeleteReservation.js
+++ react_projects/14client_wild_oasis/client_wild_oasis/app/_components/DeleteReservation.js
@@ -1,16 +1,15 @@
 "use client";
 import { TrashIcon } from "@heroicons/react/24/solid";
-import { deleteReservation } from "../_lib/actions";
 import { useTransition } from "react";
 import SpinnerMini from "./SpinnerMini";
 
-function DeleteReservation({ bookingId }) {
-  //! First time to use the useTransition hook
+function DeleteReservation({ bookingId, onDelete }) {
+	//! First time to use the useTransition hook
 	const [isPending, startTransition] = useTransition();
 
 	function handleDelete() {
 		if (confirm("Are you sure you want to delete this reservation?"))
-			startTransition(() => deleteReservation(bookingId));
+			startTransition(() => onDelete(bookingId));
 	}
 
 	return (
