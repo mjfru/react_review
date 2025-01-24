aa702ad012ffb5869a21a50024eface90463483f
Matt Frutchey <mjfru201@gmail.com>
2025-01-24 10:10:54 -0500

    add essential files for all modular demos

---
+++ reactjs_comprehensive/demos/02hooks_and_more_demo/src/App.jsx
@@ -0,0 +1,39 @@
+import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
+import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
+import UseStateGotcha from "./tutorial/01-useState/starter/05-useState-gotcha";
+import UseEffectBasics from "./tutorial/02-useEffect/starter/02-useEffect-basics";
+import MultipleEffects from "./tutorial/02-useEffect/starter/03-multiple-effects";
+import FetchData from "./tutorial/02-useEffect/starter/04-fetch-data";
+import MultipleReturnsBasics from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+import MultipleReturnsFetchData from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
+import ShortCircuitOverview from "./tutorial/03-conditional-rendering/starter/04-short-circuit-overview";
+import ShortCircuitExamples from "./tutorial/03-conditional-rendering/starter/05-short-circuit-examples";
+import ToggleChallenge from "./tutorial/03-conditional-rendering/starter/06-toggle-challenge";
+import UserChallenge from "./tutorial/03-conditional-rendering/starter/07-user-challenge";
+
+
+function App() {
+	return (
+		<div className="container">
+			<h2>Advanced React</h2>
+			{/* <ErrorExample /> */}
+			{/* <UseStateBasics /> */}
+			{/* <UseStateArray /> */}
+			{/* <UseStateObject /> */}
+			{/* <UseStateGotcha /> */}
+      {/* <UseEffectBasics /> */}
+      {/* <MultipleEffects /> */}
+      {/* <FetchData /> */}
+      {/* <MultipleReturnsBasics /> */}
+      {/* <MultipleReturnsFetchData /> */}
+      {/* <ShortCircuitOverview /> */}
+      {/* <ShortCircuitExamples /> */}
+      {/* <ToggleChallenge /> */}
+      <UserChallenge />
+		</div>
+	);
+}
+
+export default App;
