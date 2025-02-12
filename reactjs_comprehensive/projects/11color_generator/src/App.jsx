29e6f9fda1f5beccbe2af9770708662c0e8e55bc
Matt Frutchey <mjfru201@gmail.com>
2025-02-12 11:03:07 -0500

    finish color generator project

--- reactjs_comprehensive/projects/11color_generator/src/App.jsx
+++ reactjs_comprehensive/projects/11color_generator/src/App.jsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+import ColorList from "./components/ColorList";
+import Form from "./components/Form";
+
+import { ToastContainer, toast } from "react-toastify";
+import Values from "values.js";
+
 const App = () => {
-  return <h2>Color Generator Homepage</h2>;
+	const [colors, setColors] = useState(new Values("#18d49c").all(10));
+	// console.log(new Values("#f15027").all(10));
+
+	const addColor = (color) => {
+		try {
+      // console.log(color);
+			const newColors = new Values(color).all(10);
+			setColors(newColors);
+		} catch (error) {
+			console.log(error.message);
+      toast.error(error.message);
+		}
+	};
+
+	return (
+		<main>
+			<ToastContainer position="top-center" />
+			<Form addColor={addColor} />
+			<ColorList colors={colors} />
+		</main>
+	);
 };
 export default App;
