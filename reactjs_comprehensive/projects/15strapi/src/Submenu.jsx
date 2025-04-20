7865244e3295f9cfc2f49cfe3a2a7a15c874fd00
Matt Frutchey <mjfru201@gmail.com>
2025-04-20 17:57:18 -0400

    finish project, fix submenus closing

--- reactjs_comprehensive/projects/15strapi/src/Submenu.jsx
+++ reactjs_comprehensive/projects/15strapi/src/Submenu.jsx
@@ -1,12 +1,27 @@
 import { useGlobalContext } from "./Context";
 import sublinks from "./data";
+import { useRef } from "react";
 
 const Submenu = () => {
-	const { pageId } = useGlobalContext();
+	const { pageId, setPageId } = useGlobalContext();
 	const currentPage = sublinks.find((item) => item.pageId === pageId);
 
+	// const submenuContainer = useRef(null);
+
+	const handleMouseLeave = (e) => {
+		// const submenu = submenuContainer.current;
+		// console.log(submenu);
+		// const { clientX, clientY } = e;
+		// console.log(clientX, clientY);
+		setPageId(null);
+	};
+
 	return (
-		<div className={currentPage ? "submenu show-submenu" : "submenu"}>
+		<div
+			className={currentPage ? "submenu show-submenu" : "submenu"}
+			onMouseLeave={handleMouseLeave}
+			// ref={submenuContainer}
+		>
 			<h5>{currentPage?.page}</h5>
 			<div
 				className="submenu-links"
