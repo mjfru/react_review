4361061164461548d819f7f6c0998a4235742a92
Matt Frutchey <mjfru201@gmail.com>
2025-04-27 14:45:20 -0400

    add actions.js for useReducer, remove log checks

---
+++ reactjs_comprehensive/projects/16cart/src/reducer.js
@@ -0,0 +1,13 @@
+import {
+	CLEAR_CART,
+	REMOVE,
+	INCREASE,
+	DECREASE,
+	LOADING,
+	DISPLAY_ITEM,
+} from "./actions";
+
+// Reducer Function:
+export const reducer = (state, action) => {
+	return state;
+};
