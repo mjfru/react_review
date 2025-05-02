f56c3ac04b6c32e9d6f67654ab05406c218c69bc
Matt Frutchey <mjfru201@gmail.com>
2025-05-02 17:20:36 -0400

    begin exercises on performance in react

---
+++ reactjs_comprehensive/demos/02hooks_and_more_demo/src/tutorial/11-performance/starter/01-lower-state/index.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
+import Counter from "./Counter";
+const LowerState = () => {
+	const [people, setPeople] = useState(data);
+	//! Lowered to Counter.jsx
+	// const [count, setCount] = useState(0);
+
+	return (
+		<section>
+			{/* <button
+        className='btn'
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: '1rem' }}
+      >
+        count {count}
+      </button> */}
+			<Counter />
+			<List people={people} />
+		</section>
+	);
+};
+export default LowerState;
