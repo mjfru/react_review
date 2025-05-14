2666d0d16b456efd568073bed087e0b849a26830
Matt Frutchey <mjfru201@gmail.com>
2025-05-14 13:18:15 -0400

    include App.js, CSS, etc. for examples

---
+++ reactjs_comprehensive/demos/03axios/src/App.js
@@ -0,0 +1,16 @@
+import Title from "./components/Title";
+import PostRequest from "./examples/3-post-request";
+// import Setup from "./examples/1-first-request";
+// import Headers from "./examples/2-headers";
+function App() {
+	return (
+		<main>
+			<Title />
+			{/* <Setup /> */}
+      {/* <Headers /> */}
+      <PostRequest />
+		</main>
+	);
+}
+
+export default App;
