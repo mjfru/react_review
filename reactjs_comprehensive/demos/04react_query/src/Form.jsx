7cf69946bc6b9fbc3cc68d326aefc6c15a776743
Matt Frutchey <mjfru201@gmail.com>
2025-05-20 14:10:02 -0400

    utilize useMutation and useQueryClient from react query for creation of new tasks

--- reactjs_comprehensive/demos/04react_query/src/Form.jsx
+++ reactjs_comprehensive/demos/04react_query/src/Form.jsx
@@ -1,26 +1,48 @@
-import { useState } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import customFetch from "./utils";
+import { toast } from "react-toastify";
 
 const Form = () => {
-  const [newItemName, setNewItemName] = useState('');
+	const [newItemName, setNewItemName] = useState("");
+	const queryClient = useQueryClient();
+	// Used when creating or editing new content in React Query
+	const { mutate: createTask, isLoading } = useMutation({
+		// Returns a promise or error
+		mutationFn: (taskTitle) => customFetch.post("/", { title: taskTitle }),
+		onSuccess: () => {
+      // Refetches from the server by invalidating the current query.
+      // Query key must match what's in the Items.jsx
+			queryClient.invalidateQueries({ queryKey: ["tasks"] });
+			toast.success("task added");
+			setNewItemName("");
+		},
+		onError: (error) => {
+			toast.error(error.response.data.msg);
+		},
+	});
+	// console.log(result);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <h4>task bud</h4>
-      <div className='form-control'>
-        <input
-          type='text '
-          className='form-input'
-          value={newItemName}
-          onChange={(event) => setNewItemName(event.target.value)}
-        />
-        <button type='submit' className='btn'>
-          add task
-        </button>
-      </div>
-    </form>
-  );
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		createTask(newItemName);
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<h4>task bud</h4>
+			<div className="form-control">
+				<input
+					type="text "
+					className="form-input"
+					value={newItemName}
+					onChange={(e) => setNewItemName(e.target.value)}
+				/>
+				<button type="submit" className="btn" disabled={isLoading}>
+					add task
+				</button>
+			</div>
+		</form>
+	);
 };
 export default Form;
