9892c0097727f6abd3815359ce3d4a227988fd1f
Matt Frutchey <mjfru201@gmail.com>
2025-05-26 17:52:06 -0400

    begin dark / light mode functionality

--- reactjs_comprehensive/projects/17unsplash_search/src/ThemeToggle.jsx
+++ reactjs_comprehensive/projects/17unsplash_search/src/ThemeToggle.jsx
@@ -1,9 +1,19 @@
-
+import { useGlobalContext } from "./context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 const ThemeToggle = () => {
-
-
-	return <h2>Theme Toggle</h2>;
+	const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+	return (
+		<section className="toggle-container">
+			<button className="dark-toggle" onClick={toggleDarkTheme}>
+				{isDarkTheme ? (
+					<BsFillMoonFill className="toggle-icon" />
+				) : (
+					<BsFillSunFill className="toggle-icon" />
+				)}
+			</button>
+		</section>
+	);
 };
 
 export default ThemeToggle;
