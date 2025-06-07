65a94c368584737dcd0a5fe9b9dec2827f270547
Matt Frutchey <mjfru201@gmail.com>
2025-06-07 14:06:45 -0400

    finish about page, add a wrapper for the main content

--- reactjs_comprehensive/projects/19mix_master/src/pages/About.jsx
+++ reactjs_comprehensive/projects/19mix_master/src/pages/About.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
 	return (
 		<div>
-			<h2>About</h2>
-			<Link to="/">Home</Link>
+			<Wrapper>
+				<h3>About Us</h3>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Exercitationem placeat numquam dignissimos, minus impedit consectetur
+					sunt saepe qui, rerum, doloremque repellat quos! Quas modi iste
+					voluptatum quibusdam distinctio maxime veniam consequuntur, doloribus
+					omnis fugiat. At vel et, est non itaque vero quo, corrupti vitae neque
+					a perferendis? Repudiandae, saepe totam!
+				</p>
+			</Wrapper>
 		</div>
 	);
 };
