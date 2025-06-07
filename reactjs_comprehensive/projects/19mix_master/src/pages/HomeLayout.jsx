816516dd41fac25172c19cc0921c82c7e79f5225
Matt Frutchey <mjfru201@gmail.com>
2025-06-07 13:54:26 -0400

    add nested routes, practice styled components with the navbar

--- reactjs_comprehensive/projects/19mix_master/src/pages/HomeLayout.jsx
+++ reactjs_comprehensive/projects/19mix_master/src/pages/HomeLayout.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
 	return (
-		<div>
-			<h1>HomeLayout</h1>
-			<Link to="/about">About Page</Link>
-		</div>
+		<>
+      <Navbar />
+			<Outlet />
+		</>
 	);
 };
 
