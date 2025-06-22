64b1e61fda385e6e164a8d14d3800d26b2276bb6
Matt Frutchey <mjfru201@gmail.com>
2025-06-22 15:42:13 -0400

    add cartSlice and 3 components

--- reactjs_comprehensive/demos/05redux_tk_demo/src/store.js
+++ reactjs_comprehensive/demos/05redux_tk_demo/src/store.js
@@ -1,8 +1,11 @@
 // Acts as state management for our entire app, similar to useContext, it will be used to wrap around our entire application
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cart/cartSlice";
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		cart: cartReducer,
+	},
 });
 
 export default store;
