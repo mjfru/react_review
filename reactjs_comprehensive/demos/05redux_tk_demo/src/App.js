5b968a1cc076c7a56e16908518d2af248d63a3d3
Matt Frutchey <mjfru201@gmail.com>
2025-06-24 10:36:54 -0400

    add calculate totals reducer, begin work on pop-up modal

--- reactjs_comprehensive/demos/05redux_tk_demo/src/App.js
+++ reactjs_comprehensive/demos/05redux_tk_demo/src/App.js
@@ -1,9 +1,21 @@
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import Modal from "./components/Modal";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
+	const { cartItems } = useSelector((store) => store.cart);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(calculateTotals());
+	}, [cartItems]);
+
 	return (
 		<main>
+			<Modal />
 			<Navbar />
 			<CartContainer />
 		</main>
