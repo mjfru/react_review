dd86f73890a1451b90ad2b079cfd390eaacfcb39
Matt Frutchey <mjfru201@gmail.com>
2025-06-29 13:05:46 -0400

    create pages and page directory, router, and fix daiyui implementation

--- reactjs_comprehensive/projects/21ecommerce_project/src/App.jsx
+++ reactjs_comprehensive/projects/21ecommerce_project/src/App.jsx
@@ -1,9 +1,69 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import {
+	About,
+	Cart,
+	Checkout,
+	Error,
+	HomeLayout,
+	Landing,
+	Login,
+	Orders,
+	Products,
+	Register,
+	SingleProduct,
+} from "./pages/index";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{
+				index: true,
+				element: <Landing />,
+			},
+			{
+				path: "products",
+				element: <Products />,
+			},
+			{
+				path: "products/:id",
+				element: <SingleProduct />,
+			},
+			{
+				path: "cart",
+				element: <Cart />,
+			},
+			{
+				path: "about",
+				element: <About />,
+			},
+			{
+				path: "checkout",
+				element: <Checkout />,
+			},
+			{
+				path: "orders",
+				element: <Orders />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+		errorElement: <Error />,
+	},
+	{
+		path: "/register",
+		element: <Register />,
+		errorElement: <Error />,
+	},
+]);
+
 function App() {
-	return (
-		<>
-			<h1 className="text-3xl font-bold underline">Comfy Store</h1>
-		</>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
