1936e4afe283a91f17d12161cacce01a2c7b70c6
Matt Frutchey <mjfru201@gmail.com>
2025-07-13 13:37:42 -0400

    add 3 new components for sub-components of the cart, implement a grid for styling and placement

--- reactjs_comprehensive/projects/21ecommerce_project/src/pages/Cart.jsx
+++ reactjs_comprehensive/projects/21ecommerce_project/src/pages/Cart.jsx
@@ -1,7 +1,38 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { CartItemsList, SectionTitle, CartTotals } from "../components";
+
 const Cart = () => {
-  return (
-    <h1 className="text-4xl">Cart</h1>
-  )
-}
+	// Temporary!
+	const user = null;
+	const numItemsInCart = useSelector((state) => state.cartState.numItemsInCart);
+
+	if (numItemsInCart === 0) {
+		return <SectionTitle text="Your cart is empty." />;
+	}
+
+	return (
+		<>
+			<SectionTitle text="Shopping Cart" />
+			<div className="mt-8 grid gap-8 lg:grid-cols-12">
+				<div className="lg:col-span-8">
+					<CartItemsList />
+				</div>
+				<div className="lg:col-span-4 lg:pl-4">
+					<CartTotals />
+					{user ? (
+						<Link to="/checkout" className="btn btn-primary btn-block mt-8">
+							Proceed to Checkout
+						</Link>
+					) : (
+						<Link to="/login" className="btn btn-primary btn-block mt-8">
+							Please Login
+						</Link>
+					)}
+				</div>
+			</div>
+		</>
+	);
+};
 
 export default Cart;
