053edd351d374736b5ea2475b7298b0e6982ed8e
Matt Frutchey <mjfru201@gmail.com>
2025-07-14 08:14:59 -0400

    add userSlice to RTK

---
+++ reactjs_comprehensive/projects/21ecommerce_project/src/features/user/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	user: { username: "user" },
+	theme: "abyss",
+};
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		loginUser: (state, action) => {
+			console.log("login");
+		},
+		logoutUser: (state) => {
+			console.log("logout");
+		},
+		toggleTheme: (state) => {
+			console.log("toggle theme");
+		},
+	},
+});
+
+export const { loginUser, logoutUser, toggleTheme } = userSlice.actions;
+
+export default userSlice.reducer;
