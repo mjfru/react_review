05e7bcfa304eeb30602ebb835cbc123cf7f16ef6
Matt Frutchey <mjfru201@gmail.com>
2025-08-09 12:33:54 -0400

    finish truthy/falsy guard exercise, notes on instanceof and ensuing challenge

--- reactjs_comprehensive/typescript/notes/08type_guarding.ts
+++ reactjs_comprehensive/typescript/notes/08type_guarding.ts
@@ -87,3 +87,36 @@ TODO - - Define a function named printLength that takes one parameter str which 
 
 - Now you can call the printLength function with a string, null, or undefined as the argument. The function will print the length of the string if a string is provided, or 'No string provided' otherwise.
 */
+
+function printLength(str: string | null | undefined): void {
+	str ? console.log(str.length) : console.log("No string provided");
+}
+printLength("Hello");
+printLength("");
+printLength(null);
+printLength(undefined);
+
+/*
+! Instance of... Type Guard
+* The instanceof type guard is a way in TypeScript to check the specific class or constructor function of an object at runtime. It returns true if the object is an instance of the class or created by the constructor function, and false otherwise.
+
+TODO - - Start by defining the function using the function keyword followed by the function name, in this case checkInput.
+- Define the function's parameter. The function takes one parameter, input, which can be of type Date or string. This is denoted by input: Date | string.
+- Inside the function, use an if statement to check if the input is an instance of Date. This is done using the instanceof operator.
+- If the input is an instance of Date, return the year part of the date as a string. This is done by calling the getFullYear method on the input and then converting it to a string using the toString method.
+- If the input is not an instance of Date (which means it must be a string), return the input as it is.
+- After defining the function, you can use it by calling it with either a Date or a string as the argument. The function will return the year part of the date if a Date is passed, or the original string if a string is passed.
+- You can store the return value of the function in a variable and then log it to the console to see the result.
+*/
+
+function checkInput(input: Date | string): string {
+	if (input instanceof Date) {
+		return input.getFullYear().toString();
+	}
+	return input;
+}
+
+const year = checkInput(new Date());
+const randomInput = checkInput("2025");
+console.log(year);
+console.log(randomInput);
