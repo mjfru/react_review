9331b9b73df8efe7d8605e320e899fa1d095d638
Matt Frutchey <mjfru201@gmail.com>
2025-08-11 08:23:40 -0400

    finish generics notes, challenges, and examples, include a reworded introduction

--- reactjs_comprehensive/typescript/notes/09generics.ts
+++ reactjs_comprehensive/typescript/notes/09generics.ts
@@ -1,5 +1,6 @@
 /*
 ! Generics <T>
+* Generics in TypeScript are like placeholders for types. They let you write functions, classes, and interfaces that work with any data type, while still keeping type safety. Instead of hardcoding string or number, you use a generic type like <T> so the type is decided when you call it.
 */
 
 //* Here's the usual approach that will be applicable in almost every situation.
@@ -41,8 +42,49 @@ const genericString: GenericInterface<string> = {
 	},
 };
 
-async function someFunc():Promise<string> {
+async function someFunc(): Promise<string> {
 	return "Goodbye, World!";
 }
 
 const result = someFunc();
+
+//TODO Challenge - A function that takes in two parameters, a number and any type of value, and puts that number of values in an array.
+// e.g. (2, 'hi') = ['hi', 'hi'];
+
+//* This works for strings, but instead of recreating this for all primitive data types, let's use generics instead.
+function generateStringArray(length: number, value: string): string[] {
+	let result: string[] = [];
+	result = Array(length).fill(value);
+	return result;
+}
+
+function generateGenericArray<T>(length: number, value: T): Array<T> {
+	let result: T[] = [];
+	result = Array(length).fill(value);
+	return result;
+}
+
+let arrayStrings = generateGenericArray<string>(10, "hello");
+let arrayNumbers = generateGenericArray<number>(15, 100);
+console.log(arrayStrings);
+console.log(arrayNumbers);
+
+//TODO Setup multiple variable types
+function pair<T, U>(param1: T, param2: U): [T, U] {
+	return [param1, param2];
+}
+
+let pairResult = pair<number, string>(123, "hello");
+
+
+interface StoreData<T = any> {
+  data: T[];
+}
+
+const storeNumbers:StoreData<number> = {
+  data: [1, 2, 3, 4]
+}
+
+const randomStuff:StoreData = {
+  data:['random', 1]
+}
