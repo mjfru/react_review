39162846b7abeb7b5a1ee63f1bd6dcca1c5ade22
Matt Frutchey <mjfru201@gmail.com>
2025-08-22 14:15:37 -0400

    setup demo project for a task manager to test out TS in an app

---
+++ reactjs_comprehensive/typescript/demos/typescript/src/tasks.ts
@@ -0,0 +1,10 @@
+// console.log("tasks");
+
+// const btn = document.querySelector<HTMLButtonElement>(".test-btn")!;
+
+// Because it could potentially be null...
+// btn?.addEventListener();
+
+// Or, use the non-null assertion operator (the exclamation point above):
+
+// btn.disabled = true;
