a8b2ecc25de79828b208820629b17060b4c5d78a
Matt Frutchey <mjfru201@gmail.com>
2025-08-22 15:35:08 -0400

    functionality completed, demo finished

--- reactjs_comprehensive/typescript/demos/typescript/src/tasks.ts
+++ reactjs_comprehensive/typescript/demos/typescript/src/tasks.ts
@@ -8,3 +8,71 @@
 // Or, use the non-null assertion operator (the exclamation point above):
 
 // btn.disabled = true;
+
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formInput = document.querySelector<HTMLInputElement>(".form-input");
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task = {
+	description: string;
+	isCompleted: boolean;
+};
+
+// Where tasks will be stored:
+const tasks: Task[] = loadTasks();
+
+tasks.forEach(renderTask);
+
+function addTask(task: Task): void {
+	tasks.push(task);
+	console.log(tasks);
+}
+
+taskForm?.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const taskDescription = formInput?.value;
+	if (taskDescription) {
+		console.log(taskDescription);
+		// Add task to the list:
+		const task: Task = {
+			description: taskDescription,
+			isCompleted: false,
+		};
+		addTask(task);
+		// Render the tasks:
+		renderTask(task);
+		// Update local storage:
+		updateStorage();
+		formInput.value = "";
+		return;
+	}
+	alert("Please enter a description of your task.");
+});
+
+function renderTask(task: Task): void {
+	const taskElement = document.createElement("li");
+	taskElement.textContent = task.description;
+
+	// Adding a checkbox:
+	const taskCheckbox = document.createElement("input");
+	taskCheckbox.type = "checkbox";
+	taskCheckbox.checked = task.isCompleted;
+
+	// Toggle checkhox:
+	taskCheckbox.addEventListener("change", () => {
+		task.isCompleted = !task.isCompleted;
+		updateStorage();
+	});
+
+	taskElement.appendChild(taskCheckbox);
+	taskListElement?.appendChild(taskElement);
+}
+
+function updateStorage(): void {
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+	const storedTasks = localStorage.getItem("tasks");
+	return storedTasks ? JSON.parse(storedTasks) : [];
+}
