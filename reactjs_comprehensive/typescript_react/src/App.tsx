6219373400e2deef3354895f4664951b9d395674
Matt Frutchey <mjfru201@gmail.com>
2025-08-23 13:09:57 -0400

    add example for using props and PropsWithChildren from React

--- reactjs_comprehensive/typescript_react/src/App.tsx
+++ reactjs_comprehensive/typescript_react/src/App.tsx
@@ -1,10 +1,13 @@
-import Component from "./starter/01-return";
+// import Component from "./starter/01-return";
+import Component from "./starter/02-props";
 
 function App() {
 	return (
 		<main>
 			{/* <h2>React & Typescript</h2> */}
-			<Component />
+			<Component name="Peter" id={123}>
+        <h2>Hello there!</h2>
+      </Component>
 		</main>
 	);
 }
--- reactjs_comprehensive/typescript_react/src/starter/02-props/index.tsx
+++ reactjs_comprehensive/typescript_react/src/starter/02-props/index.tsx
@@ -1,9 +1,25 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
-    </div>
-  );
+import { type PropsWithChildren } from "react";
+
+type ComponentProps = PropsWithChildren<{
+	name: string;
+	id: number;
+}>;
+// type ComponentProps = {
+// 	name: string;
+// 	id: number;
+// 	children?: React.ReactNode;
+// };
+
+function Component({ name, id, children }: ComponentProps) {
+	return (
+		<div>
+			<h2>React & Typescript</h2>
+			<h2>Props</h2>
+			<h3>
+				Name: {name} - ID: {id}
+			</h3>
+			{children}
+		</div>
+	);
 }
 export default Component;
