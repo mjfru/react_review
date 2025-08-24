f25febcdfe46c6553069fe59cb85db7e0d230e43
Matt Frutchey <mjfru201@gmail.com>
2025-08-24 14:53:10 -0400

    finish event handler example

--- reactjs_comprehensive/typescript_react/src/starter/04-events/index.tsx
+++ reactjs_comprehensive/typescript_react/src/starter/04-events/index.tsx
@@ -1,31 +1,46 @@
 import { useState } from "react";
 
+type Person = {
+	name: string;
+};
+
 function Component() {
 	const [text, setText] = useState("");
 	const [email, setEmail] = useState("");
+
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		console.log(e.target.value);
 		setEmail(e.target.value);
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		// const formData = new FormData(e.target as HTMLFormElement);
+		const data = Object.fromEntries(formData);
+		console.log(data);
+		const text = formData.get("text") as string;
+		const person: Person = { name: data.text as string };
 	};
 
 	return (
 		<section>
 			<h2>React & Typescript</h2>
 			<h2>Events</h2>
-			<form action="" className="form">
+			<form action="" onSubmit={handleSubmit} className="form">
 				<input
 					type="text"
 					className="form-input mb-1"
 					value={text}
 					onChange={(e) => setText(e.target.value)}
-          name="text"
+					name="text"
 				/>
 				<input
 					type="email"
 					className="form-input mb-1"
 					value={email}
 					onChange={handleChange}
-          name="email"
+					name="email"
 				/>
 				<button type="submit" className="btn btn-block">
 					Submit
