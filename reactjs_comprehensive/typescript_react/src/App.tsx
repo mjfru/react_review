4f87c00dd36f9119f847b17224925f3e60771130
Matt Frutchey <mjfru201@gmail.com>
2025-08-26 11:04:12 -0400

    include fetch examples with Axios and Zod along with TS

--- reactjs_comprehensive/typescript_react/src/App.tsx
+++ reactjs_comprehensive/typescript_react/src/App.tsx
@@ -4,7 +4,8 @@
 // import Component from "./starter/04-events";
 // import Component from "./starter/05-challenge";
 // import Component from "./starter/06-context";
-import Component from "./starter/07-reducers";
+// import Component from "./starter/07-reducers";
+import Component from "./starter/08-fetch-data";
 
 function App() {
 	return (
@@ -29,7 +30,10 @@ function App() {
 			{/* <Component /> */}
 
 			{/* Reducers */}
-			<Component />
+			{/* <Component /> */}
+
+      {/* Fetching Data */}
+      <Component />
 		</main>
 	);
 }
--- reactjs_comprehensive/typescript_react/src/starter/08-fetch-data/index.tsx
+++ reactjs_comprehensive/typescript_react/src/starter/08-fetch-data/index.tsx
@@ -1,9 +1,93 @@
+// import { useState, useEffect } from "react";
+// const url = "https://www.course-api.com/react-tours-project";
+
+// import { type Tour, tourSchema } from "./utils";
+
+// function Component() {
+// 	// Tours
+// 	const [isLoading, setIsLoading] = useState(false);
+// 	const [isError, setIsError] = useState<string | null>(null);
+
+// 	const [tours, setTours] = useState<Tour[]>([]);
+
+// 	useEffect(() => {
+// 		const fetchData = async () => {
+// 			setIsLoading(true);
+
+// 			try {
+// 				const response = await fetch(url);
+// 				if (!response.ok) {
+// 					throw new Error(`Failed to fetch tours...`);
+// 				}
+// 				const rawData: Tour[] = await response.json();
+// 				const result = tourSchema.array().safeParse(rawData);
+// 				if (!result.success) {
+// 					console.log(result.error.message);
+// 					throw new Error(`Failed to parse tours.`);
+// 				}
+// 				setTours(result.data);
+// 			} catch (error) {
+// 				const message =
+// 					error instanceof Error ? error.message : "There was an error...";
+// 				setIsError(message);
+// 			} finally {
+// 				setIsLoading(false);
+// 			}
+// 		};
+// 		fetchData();
+// 	}, []);
+
+// 	if (isLoading) {
+// 		return <h3>Loading...</h3>;
+// 	}
+// 	if (isError) {
+// 		return <h3>Error {isError}</h3>;
+// 	}
+
+// 	return (
+// 		<div>
+// 			<h2 className="mb-1">Tours</h2>
+// 			{tours.map((tour) => {
+// 				return (
+// 					<p key={tour.id} className="mb-1">
+// 						{tour.name}
+// 					</p>
+// 				);
+// 			})}
+// 		</div>
+// 	);
+// }
+// export default Component;
+
+import { fetchTours } from "./utils";
+import { useQuery } from "@tanstack/react-query";
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Fetch Data</h2>
-    </div>
-  );
+	const {
+		isPending,
+		isError,
+		error,
+		data: tours,
+	} = useQuery({
+		queryKey: ["tours"],
+		queryFn: fetchTours,
+	});
+
+	if (isPending) return <h2>Loading...</h2>;
+	if (isError) return <h2>Error: {error.message}</h2>;
+
+	return (
+		<div>
+			<h2 className="mb-1">Tours</h2>
+			{tours.map((tour) => {
+				return (
+					<p className="mb-1" key={tour.id}>
+						{tour.name}
+					</p>
+				);
+			})}
+		</div>
+	);
 }
+
 export default Component;
