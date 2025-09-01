467ebe00ee2e02589f51ccf761cb5df2db5978bc
Matt Frutchey <mjfru201@gmail.com>
2025-09-01 13:07:45 -0400

    include practice with local images and Next's <Image>

--- reactjs_comprehensive/nextjs/demos/tutorial/app/tours/[id]/page.tsx
+++ reactjs_comprehensive/nextjs/demos/tutorial/app/tours/[id]/page.tsx
@@ -1,8 +1,27 @@
+import riga from "@/images/riga.jpg";
+import Image from "next/image";
+
 function page({ params }: { params: { id: string } }) {
 	const { id } = params;
 	return (
 		<div>
 			<h1 className="text-4xl">ID: {id}</h1>
+			<section className="flex gap-x-4 mt-4">
+				{/* Local Image Example */}
+				<div>
+					<Image
+						src={riga}
+						alt="Riga"
+						priority
+						width={192}
+						height={192}
+						className="w-48 h-48 object-cover rounded"
+					/>
+					<h2>Local Image</h2>
+				</div>
+				{/* Remote-hosted Image Example */}
+				<div></div>
+			</section>
 		</div>
 	);
 }
