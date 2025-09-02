4a8f81bf4018e9d0745d1a9f8d30ee500110a2ff
Matt Frutchey <mjfru201@gmail.com>
2025-09-02 11:06:12 -0400

    nested route examples-- auth/sign-in

--- reactjs_comprehensive/nextjs/demos/tutorial/app/tours/[id]/page.tsx
+++ reactjs_comprehensive/nextjs/demos/tutorial/app/tours/[id]/page.tsx
@@ -1,6 +1,8 @@
 import riga from "@/images/riga.jpg";
 import Image from "next/image";
 
+const url = "https://www.course-api.com/images/tours/tour-1.jpeg";
+
 function page({ params }: { params: { id: string } }) {
 	const { id } = params;
 	return (
@@ -20,7 +22,18 @@ function page({ params }: { params: { id: string } }) {
 					<h2>Local Image</h2>
 				</div>
 				{/* Remote-hosted Image Example */}
-				<div></div>
+				{/* The width and height props are needed for remote images */}
+				<div>
+					<Image
+						src={url}
+						alt="tour"
+						width={192}
+						height={192}
+						priority
+						className="w-48 h-48 object-cover rounded"
+					/>
+          <h2>Remote Image</h2>
+				</div>
 			</section>
 		</div>
 	);
