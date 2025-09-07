86e3dea6503dd3a5073405518d1170b12b32d341
Matt Frutchey <mjfru201@gmail.com>
2025-09-07 15:38:36 -0400

    utilize NextRequest and NextResponse and create a POST route

--- reactjs_comprehensive/nextjs/demos/tutorial/app/api/users/route.ts
+++ reactjs_comprehensive/nextjs/demos/tutorial/app/api/users/route.ts
@@ -1,6 +1,19 @@
 import { fetchUsers, saveUser } from "@/utils/actions";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
+	console.log(request.url);
+	console.log(request.nextUrl.searchParams.get("id"));
+
 	const users = await fetchUsers();
 	return Response.json({ users });
+};
+
+export const POST = async (request: Request) => {
+	const user = await request.json();
+	console.log(user);
+
+	const newUser = { ...user, id: Date.now().toString() };
+	await saveUser(newUser);
+	return Response.json({ message: "user created" });
 };
