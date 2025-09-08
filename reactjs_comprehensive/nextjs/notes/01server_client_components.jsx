2a0a524770872fc52a113680b9e71b55738c4a56
Matt Frutchey <mjfru201@gmail.com>
2025-09-08 13:02:15 -0400

    include a small note regarding server vs client components

---
+++ reactjs_comprehensive/nextjs/notes/01server_client_components.jsx
@@ -0,0 +1,14 @@
+/*
+! Server Components
+* All components in Next.js use server components...components that rendered on the server, not on the client's browser
+
+Benefits:
+  - Data fetching
+  - Security
+  - Caching
+  - Bundle Size
+
+! Client Components
+* These use the browser's API and allow users to interact or manipulate the component directly.
+! Client components must be marked, preferably at the top, use "use client" (yes, as a string like that).
+*/
