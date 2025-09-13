be9ef28eec42f5711b83d69d89249b87e30c55fc
Matt Frutchey <mjfru201@gmail.com>
2025-09-13 07:45:10 -0400

    add cartbutton, logo, and navsearch components

--- reactjs_comprehensive/nextjs/projects/store/components/navbar/NavSearch.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/navbar/NavSearch.tsx
@@ -1,9 +1,13 @@
-
+import { Input } from "../ui/input";
 
 function NavSearch() {
-  return (
-    <div>NavSearch</div>
-  )
+	return (
+		<Input
+			type="search"
+			placeholder="Search products..."
+			className="max-w-xs dark:bg-muted"
+		></Input>
+	);
 }
 
-export default NavSearch
+export default NavSearch;
