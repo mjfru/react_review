ab7d357d3545561b0335b1ca1b5275bba20e72e6
Matt Frutchey <mjfru201@gmail.com>
2025-09-20 15:31:37 -0400

    practice crud operations with Prisma, initialize seed data for future work

--- reactjs_comprehensive/nextjs/projects/store/app/about/page.tsx
+++ reactjs_comprehensive/nextjs/projects/store/app/about/page.tsx
@@ -1,5 +1,27 @@
+// import db from "@/utils/db";
+
+// async 
 function About() {
-	return <div>About</div>;
+	// const profile = await db.testProfile.create({
+	// 	data: {
+	// 		name: "random name",
+	// 	},
+	// });
+
+	// const users = await db.testProfile.findMany();
+	
+  return (
+		<div>
+      About Page
+			{/* {users.map((user) => {
+				return (
+					<h2 key={user.id} className="text-2xl font-bold">
+						{user.name}
+					</h2>
+				);
+			})} */}
+		</div>
+	);
 }
 
 export default About;
