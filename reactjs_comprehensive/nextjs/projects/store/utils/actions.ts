d86c5cb80cb7de2fc3d9be4e6f147cc7e0d36e20
Matt Frutchey <mjfru201@gmail.com>
2025-09-21 14:50:00 -0400

    add a new utils file to hold crud actions, create retrievals for featured products and all products

---
+++ reactjs_comprehensive/nextjs/projects/store/utils/actions.ts
@@ -0,0 +1,21 @@
+import db from "@/utils/db";
+
+export const fetchFeaturedProducts = async () => {
+	const products = await db.product.findMany({
+		where: {
+			featured: true,
+		},
+		// select: {
+		// 	name: true,
+		// },
+	});
+	return products;
+};
+
+export const fetchAllProducts = async () => {
+	return db.product.findMany({
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+};
