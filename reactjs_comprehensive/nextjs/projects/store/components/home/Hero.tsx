20c50c23b395b6de0f008fb468237226c3bd568f
Matt Frutchey <mjfru201@gmail.com>
2025-09-22 13:37:56 -0400

    use stock images for the hero carousel and finish the overall homepage hero section

--- reactjs_comprehensive/nextjs/projects/store/components/home/Hero.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/home/Hero.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import HeroCarousel from "./HeroCarousel";
+import { Button } from "../ui/button";
+
 function Hero() {
-	return <div>Hero</div>;
+	return (
+		<section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+			<div>
+				<h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+					We are changing the way people shop
+				</h1>
+				<p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+					suscipit necessitatibus fugiat maiores aliquam voluptatum quae illo
+					quo ipsum beatae.
+				</p>
+				<Button asChild size="lg" className="mt-10">
+					<Link href="/products">Our Products</Link>
+				</Button>
+			</div>
+			<HeroCarousel />
+		</section>
+	);
 }
 
 export default Hero;
--- reactjs_comprehensive/nextjs/projects/store/components/home/HeroCarousel.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/home/HeroCarousel.tsx
@@ -1,5 +1,46 @@
+import hero1 from "../../public/images/hero1.jpg";
+import hero2 from "../../public/images/hero2.jpg";
+import hero3 from "../../public/images/hero3.jpg";
+import hero4 from "../../public/images/hero4.jpg";
+
+import Image from "next/image";
+import { Card, CardContent } from "../ui/card";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "../ui/carousel";
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
 function HeroCarousel() {
-	return <div>HeroCarousel</div>;
+	return (
+		<div className="hidden lg:block">
+			<Carousel>
+				<CarouselContent>
+					{carouselImages.map((image, index) => {
+						return (
+							<CarouselItem key={index}>
+								<Card>
+									<CardContent className="">
+										<Image
+											src={image}
+											alt="Hero"
+											className="w-full h-[24rem] rounded-md object-cover"
+										/>
+									</CardContent>
+								</Card>
+							</CarouselItem>
+						);
+					})}
+				</CarouselContent>
+				<CarouselPrevious className="cursor-pointer" />
+				<CarouselNext className="cursor-pointer" />
+			</Carousel>
+		</div>
+	);
 }
 
 export default HeroCarousel;
