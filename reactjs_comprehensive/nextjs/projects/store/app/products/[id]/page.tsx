ef6f5a656fb40cb34657a2ae9b6050f5a5023445
Matt Frutchey <mjfru201@gmail.com>
2025-09-26 08:55:15 -0400

    add clerk for authentication purposes

---
+++ reactjs_comprehensive/nextjs/projects/store/app/products/[id]/page.tsx
@@ -0,0 +1,50 @@
+import Breadcrumbs from "@/components/single-product/Breadcrumbs";
+import { fetchSingleProduct } from "@/utils/actions";
+import Image from "next/image";
+import { formatCurrency } from "@/utils/format";
+import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
+import AddToCart from "@/components/single-product/AddToCart";
+import ProductRating from "@/components/single-product/ProductRating";
+
+async function SingleProductPage({ params }: { params: { id: string } }) {
+	const product = await fetchSingleProduct(params.id);
+	const { name, image, company, description, price } = product;
+	const dollarsAmount = formatCurrency(price);
+
+	return (
+		<section>
+			<Breadcrumbs name={product.name} />
+			<div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+				{/* Image First Column */}
+				<div className="relative h-full">
+					<Image
+						src={image}
+						alt={name}
+						fill
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+						priority
+						className="w-full rounded object-cover"
+					/>
+				</div>
+				{/* Product Info Second Column */}
+				<div>
+					<div className="flex gap-x-8 items-center">
+						<h1 className="capitalize text-3xl font-bold">{name}</h1>
+						<FavoriteToggleButton productId={params.id} />
+					</div>
+          <ProductRating productId={params.id} />
+          <h4 className="text-xl mt-2">
+            {company}
+          </h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded">{dollarsAmount}</p>
+          <p className="mt-6 leading-8 text-muted-foreground">
+            {description}
+          </p>
+          <AddToCart productId={params.id}/>
+        </div>
+			</div>
+		</section>
+	);
+}
+
+export default SingleProductPage;
