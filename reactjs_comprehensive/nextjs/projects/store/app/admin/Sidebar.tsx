0864bebfeaefe522b61ffcbf72c54aa28da9eb08
Matt Frutchey <mjfru201@gmail.com>
2025-09-28 13:47:20 -0400

    begin setting up admin routes, a sidebar for the dashboard

---
+++ reactjs_comprehensive/nextjs/projects/store/app/admin/Sidebar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { adminLinks } from "@/utils/links";
+
+function Sidebar() {
+	// Gives us the value of the path:
+	const pathname = usePathname();
+
+	return (
+		<aside>
+			{adminLinks.map((link) => {
+				const isActivePage = pathname === link.href;
+				const variant = isActivePage ? "default" : "ghost";
+				return (
+					<Button
+						asChild
+						className="w-full mb-2 capitalize font-normal "
+						variant={variant}
+						// ?
+						key={link.href}
+					>
+						<Link href={link.href}>{link.label}</Link>
+					</Button>
+				);
+			})}
+		</aside>
+	);
+}
+
+export default Sidebar;
