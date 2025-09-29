8376f8f61ede53e200fe8dd8df07baa050e048b4
Matt Frutchey <mjfru201@gmail.com>
2025-09-29 14:39:21 -0400

    finish more smaller components for the form, including a submit button, text area, image input, etc.

--- reactjs_comprehensive/nextjs/projects/store/app/admin/products/create/page.tsx
+++ reactjs_comprehensive/nextjs/projects/store/app/admin/products/create/page.tsx
@@ -1,7 +1,7 @@
 import FormInput from "@/components/form/FormInput";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 
 import { faker } from "@faker-js/faker";
 
--- reactjs_comprehensive/nextjs/projects/store/components/form/Buttons.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/form/Buttons.tsx
@@ -1,7 +1,42 @@
-function Buttons() {
-  return (
-    <div>Buttons</div>
-  )
-}
+"use client";
 
-export default Buttons
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { LuTrash2, LuPen } from "react-icons/lu";
+import { ReloadIcon } from "@radix-ui/react-icons";
+
+type btnSize = "default" | "lg" | "sm";
+
+type SubmitButtonProps = {
+	className?: string;
+	text?: string;
+	size?: btnSize;
+};
+
+export function SubmitButton({
+	className = "",
+	text = "submit",
+	size = "lg",
+}: SubmitButtonProps) {
+	const { pending } = useFormStatus();
+	return (
+		<Button
+			type="submit"
+			disabled={pending}
+			className={cn("capitalize", className)}
+			size={size}
+		>
+			{pending ? (
+				<>
+					<ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+					Please wait...
+				</>
+			) : (
+				text
+			)}
+		</Button>
+	);
+}
