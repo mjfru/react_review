42af69ae8515eeade9328aca0b4084e7e298989d
Matt Frutchey <mjfru201@gmail.com>
2025-10-13 13:13:24 -0400

    add utility actions for fetching favorites, logic for being an authorized user and favoriting items

--- reactjs_comprehensive/nextjs/projects/store/components/form/Buttons.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/form/Buttons.tsx
@@ -7,7 +7,7 @@ import { SignInButton } from "@clerk/nextjs";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { LuTrash2, LuPen } from "react-icons/lu";
 import { ReloadIcon } from "@radix-ui/react-icons";
-import { LucidePenSquare } from "lucide-react";
+// import { LucidePenSquare } from "lucide-react";
 
 type btnSize = "default" | "lg" | "sm";
 
@@ -69,3 +69,37 @@ export const IconButton = ({ actionType }: { actionType: actionType }) => {
 	);
 };
 
+export const CardSignInButton = () => {
+	return (
+		<SignInButton mode="modal">
+			<Button
+				type="button"
+				size="icon"
+				variant="outline"
+				className="p-2 cursor-pointer"
+			>
+				<FaRegHeart />
+			</Button>
+		</SignInButton>
+	);
+};
+
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+	const { pending } = useFormStatus();
+	return (
+		<Button
+			type="submit"
+			size="icon"
+			variant="outline"
+			className="p-2 cursor-pointer"
+		>
+			{pending ? (
+				<ReloadIcon className="animate-spin" />
+			) : isFavorite ? (
+				<FaHeart />
+			) : (
+				<FaRegHeart />
+			)}
+		</Button>
+	);
+};
--- reactjs_comprehensive/nextjs/projects/store/components/products/FavoriteToggleForm.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/products/FavoriteToggleForm.tsx
@@ -1,9 +1,31 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { toggleFavoriteAction } from "@/utils/actions";
+import FormContainer from "../form/FormContainer";
+import { CardSubmitButton } from "../form/Buttons";
 
+type FavoriteToggleFormProps = {
+	productId: string;
+	favoriteId: string | null;
+};
 
-function FavoriteToggleForm() {
-  return (
-    <div>FavoriteToggleForm</div>
-  )
+function FavoriteToggleForm({
+	productId,
+	favoriteId,
+}: FavoriteToggleFormProps) {
+	const pathname = usePathname();
+
+	const toggleAction = toggleFavoriteAction.bind(null, {
+		productId,
+		favoriteId,
+		pathname,
+	});
+
+	return (
+		<FormContainer action={toggleAction}>
+			<CardSubmitButton isFavorite={favoriteId ? true : false} />
+		</FormContainer>
+	);
 }
 
-export default FavoriteToggleForm
+export default FavoriteToggleForm;
