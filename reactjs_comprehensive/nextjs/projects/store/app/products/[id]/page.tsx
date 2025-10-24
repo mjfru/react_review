8c09d3891baeda34b7c42d99d68b17a31d11ca01
Matt Frutchey <mjfru201@gmail.com>
2025-10-24 15:00:07 -0400

    incorporate new Review-related components into the SingleProductPage

--- reactjs_comprehensive/nextjs/projects/store/app/products/[id]/page.tsx
+++ reactjs_comprehensive/nextjs/projects/store/app/products/[id]/page.tsx
@@ -5,6 +5,9 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import ProductReview from "@/components/reviews/ProductReview";
+
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
 	const product = await fetchSingleProduct(params.id);
@@ -43,6 +46,8 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
           {/* <AddToCart productId={params.id}/> */}
         </div>
 			</div>
+      <ProductReview productId={params.id} />
+      <SubmitReview productId={params.id} />
 		</section>
 	);
 }
