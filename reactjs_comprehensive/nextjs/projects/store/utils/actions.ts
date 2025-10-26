4f1a4994f74b205b8367cc6b085554bb66c48dee
Matt Frutchey <mjfru201@gmail.com>
2025-10-26 17:23:30 -0400

    create model for the cart and cart products, add functionality to show number of items in cart on navbar

--- reactjs_comprehensive/nextjs/projects/store/utils/actions.ts
+++ reactjs_comprehensive/nextjs/projects/store/utils/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import db from "@/utils/db";
-import { currentUser, getAuth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import {
 	imageSchema,
@@ -345,3 +345,30 @@ export const findExistingReview = async (userId: string, productId: string) => {
 		},
 	});
 };
+
+export const fetchCartItems = async () => {
+	const { userId } = await auth();
+	const cart = await db.cart.findFirst({
+		where: {
+			clerkId: userId ?? "",
+		},
+		select: {
+			numItemsInCart: true,
+		},
+	});
+	return cart?.numItemsInCart || 0;
+};
+
+const fetchProduct = async () => {};
+
+export const fetchOrCreateCart = async () => {};
+
+const updateOrCreateCartItem = async () => {};
+
+export const updateCart = async () => {};
+
+export const addToCartAction = async () => {};
+
+export const removeCartItemAction = async () => {};
+
+export const updateCartItemAction = async () => {};
