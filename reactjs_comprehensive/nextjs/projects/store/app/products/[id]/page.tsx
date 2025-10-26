746b40519ac81219c9d685f62ae8cb2d04d8990f
Matt Frutchey <mjfru201@gmail.com>
2025-10-26 13:18:26 -0400

    finish actions relating to reviews, add conditional loading to see if the user has submitted one already

--- reactjs_comprehensive/nextjs/projects/store/app/products/[id]/page.tsx
+++ reactjs_comprehensive/nextjs/projects/store/app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Breadcrumbs from "@/components/single-product/Breadcrumbs";
-import { fetchSingleProduct } from "@/utils/actions";
+import { fetchSingleProduct, findExistingReview } from "@/utils/actions";
 import Image from "next/image";
 import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
@@ -7,12 +7,15 @@ import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
 import SubmitReview from "@/components/reviews/SubmitReview";
 import ProductReview from "@/components/reviews/ProductReview";
-
+import { auth } from "@clerk/nextjs/server";
 
 async function SingleProductPage({ params }: { params: { id: string } }) {
 	const product = await fetchSingleProduct(params.id);
 	const { name, image, company, description, price } = product;
 	const dollarsAmount = formatCurrency(price);
+	const { userId } = await auth();
+	const reviewDoesNotExist =
+		userId && !(await findExistingReview(userId, product.id));
 
 	return (
 		<section>
@@ -35,19 +38,18 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
 						<h1 className="capitalize text-3xl font-bold">{name}</h1>
 						<FavoriteToggleButton productId={params.id} />
 					</div>
-          <ProductRating productId={params.id} />
-          <h4 className="text-xl mt-2">
-            {company}
-          </h4>
-          <p className="mt-3 text-md bg-muted inline-block p-2 rounded">{dollarsAmount}</p>
-          <p className="mt-6 leading-8 text-muted-foreground">
-            {description}
-          </p>
-          {/* <AddToCart productId={params.id}/> */}
-        </div>
+					<ProductRating productId={params.id} />
+					<h4 className="text-xl mt-2">{company}</h4>
+					<p className="mt-3 text-md bg-muted inline-block p-2 rounded">
+						{dollarsAmount}
+					</p>
+					<p className="mt-6 leading-8 text-muted-foreground">{description}</p>
+					{/* <AddToCart productId={params.id}/> */}
+				</div>
 			</div>
-      <ProductReview productId={params.id} />
-      <SubmitReview productId={params.id} />
+			<ProductReview productId={params.id} />
+
+			{reviewDoesNotExist && <SubmitReview productId={params.id} />}
 		</section>
 	);
 }
