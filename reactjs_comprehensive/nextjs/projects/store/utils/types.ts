6f3ed3c6a46680bac25f6c203b6fbb180a8df1d6
Matt Frutchey <mjfru201@gmail.com>
2025-10-30 12:51:54 -0400

    display informational columns on checkout/cart page

--- reactjs_comprehensive/nextjs/projects/store/utils/types.ts
+++ reactjs_comprehensive/nextjs/projects/store/utils/types.ts
@@ -1,22 +1,28 @@
+import { Prisma } from "@prisma/client";
+
+export type CartItemWithProduct = Prisma.CartItemGetPayload<{
+	include: { product: true };
+}>;
+
 export type actionFunction = (
-  prevState: unknown,
-  formData: FormData
+	prevState: unknown,
+	formData: FormData
 ) => Promise<{ message: string }>;
 
 export type CartItem = {
-  productId: string;
-  image: string;
-  title: string;
-  price: string;
-  amount: number;
-  company: string;
+	productId: string;
+	image: string;
+	title: string;
+	price: string;
+	amount: number;
+	company: string;
 };
 
 export type CartState = {
-  cartItems: CartItem[];
-  numItemsInCart: number;
-  cartTotal: number;
-  shipping: number;
-  tax: number;
-  orderTotal: number;
+	cartItems: CartItem[];
+	numItemsInCart: number;
+	cartTotal: number;
+	shipping: number;
+	tax: number;
+	orderTotal: number;
 };
