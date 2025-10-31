38680890d9068545e54c578e3058051c6955511c
Matt Frutchey <mjfru201@gmail.com>
2025-10-31 15:12:18 -0400

    add functionality to change amount on cart page

--- reactjs_comprehensive/nextjs/projects/store/components/cart/ThirdColumn.tsx
+++ reactjs_comprehensive/nextjs/projects/store/components/cart/ThirdColumn.tsx
@@ -5,20 +5,31 @@ import { Mode } from "../single-product/SelectProductAmount";
 import FormContainer from "../form/FormContainer";
 import { SubmitButton } from "../form/Buttons";
 import { removeCartItemAction, updateCartItemAction } from "@/utils/actions";
-import { useToast } from "../ui/use-toast";
 
 function ThirdColumn({ quantity, id }: { quantity: number; id: string }) {
 	const [amount, setAmount] = useState(quantity);
+	const [isLoading, setIsLoading] = useState(false);
+
 	const handleAmountChange = async (value: number) => {
+		setIsLoading(true);
+
+		const result = await updateCartItemAction({
+			amount: value,
+			cartItemId: id,
+		});
+
 		setAmount(value);
+
+		setIsLoading(false);
 	};
+
 	return (
 		<div className="md:ml-8">
 			<SelectProductAmount
 				amount={amount}
 				setAmount={handleAmountChange}
 				mode={Mode.CartItem}
-				isLoading={false}
+				isLoading={isLoading}
 			/>
 			<FormContainer action={removeCartItemAction}>
 				<input type="hidden" name="id" value={id} />
