836e4542022f8baddeca6a89860c8d22375ca237
Matt Frutchey <mjfru201@gmail.com>
2025-11-13 12:01:36 -0500

    finalize forked and popular repo charts, add a loader, and update components in user profile

--- reactjs_comprehensive/nextjs/projects/graphql_project/src/components/charts/ForkedRepos.tsx
+++ reactjs_comprehensive/nextjs/projects/graphql_project/src/components/charts/ForkedRepos.tsx
@@ -1,11 +1,44 @@
 import { Repository } from "@/types";
 import { calculateMostForkedRepos } from "@/utils";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+	ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "@/components/ui/chart";
 
 function ForkedRepos({ repositories }: { repositories: Repository[] }) {
-  const mostForkedRepos = calculateMostForkedRepos(repositories);
-  // console.log(mostForkedRepos);
-  
-	return <div>ForkedRepos</div>;
+	const chartConfig = {
+		repo: {
+			label: "Repository",
+			color: "#FACD12",
+		},
+	} satisfies ChartConfig;
+
+	const mostForkedRepos = calculateMostForkedRepos(repositories);
+	// console.log(mostForkedRepos);
+
+	return (
+		<div>
+			<h2 className="text-2xl font-semibold text-center mb-4">Forked Repos</h2>
+			<ChartContainer config={chartConfig} className="h-100 w-full">
+				<BarChart accessibilityLayer data={mostForkedRepos}>
+					<CartesianGrid vertical={false} />
+					<XAxis
+						dataKey="repo"
+						tickLine={false}
+						tickMargin={10}
+						axisLine={false}
+						tickFormatter={(value) => value.slice(0, 10)}
+					/>
+					<YAxis dataKey="count" />
+					<ChartTooltip content={<ChartTooltipContent />} />
+					<Bar dataKey="count" fill="var(--color-repo)" radius={4} />
+				</BarChart>
+			</ChartContainer>
+		</div>
+	);
 }
 
 export default ForkedRepos;
