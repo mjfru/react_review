30bad4cf4a131ed0ded782c2cfe2ad8f160a7f89
Matt Frutchey <mjfru201@gmail.com>
2025-11-22 12:47:46 -0500

    add environment for search-by-role testing

--- reactjs_comprehensive/testing/01rtl_intro/src/tutorial/03-search-by-role/Sandbox.tsx
+++ reactjs_comprehensive/testing/01rtl_intro/src/tutorial/03-search-by-role/Sandbox.tsx
@@ -1,4 +1,35 @@
+import { useEffect, useState } from "react";
+
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+	const [showAsyncButton, setShowAsyncButton] = useState(false);
+	const [showError, setError] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAsyncButton(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [])
+
+	return <div>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </nav>
+    {/* Headings */}
+    <h1>Main Heading</h1>
+    <h2>Subheading</h2>
+    <img src="example.jpg" alt="Example" />
+
+    {/* Regular Buttons */}
+    <button>Click Me</button>
+    <button>Submit</button>
+    <button>Cancel</button>
+
+    {/* Conditional - Show Error */}
+    {showError && <button>Error</button>}
+    {/* Conditional - Async Button */}
+    {showAsyncButton && <button>Async Button</button>}
+  </div>;
 };
 export default Sandbox;
