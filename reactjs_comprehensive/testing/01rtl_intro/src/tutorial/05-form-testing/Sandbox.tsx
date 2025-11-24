096c14d76694d6952f72ef7df407f9cd5f4aa918
Matt Frutchey <mjfru201@gmail.com>
2025-11-24 08:58:36 -0500

    create a working form for input/form testing

--- reactjs_comprehensive/testing/01rtl_intro/src/tutorial/05-form-testing/Sandbox.tsx
+++ reactjs_comprehensive/testing/01rtl_intro/src/tutorial/05-form-testing/Sandbox.tsx
@@ -1,4 +1,91 @@
-const Sandbox = () => {
-  return <div>Sandbox</div>;
+import { useState } from "react";
+import validator from "validator";
+
+const defaultStates = {
+	email: "",
+	password: "",
+	confirmPassword: "",
 };
+
+const Sandbox = () => {
+	const labelStyles = "block text-grey-700 font-medium mb-2";
+	const inputStyles = "w-full px-3 py-2 border border-grey-300 rounded-md";
+	const buttonStyles =
+		"w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600";
+
+	const [signupInput, setSignupInput] = useState(defaultStates);
+
+	const [error, setError] = useState("");
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, value } = e.target;
+		setSignupInput({ ...signupInput, [id]: value });
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		if (!validator.isEmail(signupInput.email)) {
+			return setError("Invalid email address");
+		}
+		if (!validator.isLength(signupInput.password, { min: 5 })) {
+			return setError("Password must be at least 5 characters.");
+		}
+		if (signupInput.password !== signupInput.confirmPassword) {
+			return setError("Passwords must match.");
+		}
+		setError("");
+		setSignupInput(defaultStates);
+	};
+
+	return (
+		<div className="container mx-auto max-w-md mt-10 p-6 bg-white rounded-lg shadow-md">
+			<form className="space-y-4">
+				{/* Email */}
+				<div className="mb-3">
+					<label htmlFor="email" className={labelStyles}>
+						Email Address
+					</label>
+					<input
+						type="email"
+						id="email"
+						value={signupInput.email}
+						onChange={handleChange}
+						className={inputStyles}
+					/>
+				</div>
+				{/* Password */}
+				<div className="mb-3">
+					<label htmlFor="password" className={labelStyles}>
+						Password
+					</label>
+					<input
+						type="password"
+						id="password"
+						value={signupInput.password}
+						onChange={handleChange}
+						className={inputStyles}
+					/>
+				</div>
+				{/* Confirm Password */}
+				<div className="mb-3">
+					<label htmlFor="confirmPassword" className={labelStyles}>
+						Confirm Password
+					</label>
+					<input
+						type="password"
+						id="confirmPassword"
+						value={signupInput.confirmPassword}
+						onChange={handleChange}
+						className={inputStyles}
+					/>
+				</div>
+				{error && <p className="text-red-500 text-sm">{error}</p>}
+				<button type="button" onClick={handleSubmit} className={buttonStyles}>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
+
 export default Sandbox;
