2e6b165f4998b6243390d0677c9839230aa48ba9
Matt Frutchey <mjfru201@gmail.com>
2025-12-14 13:05:13 -0500

    add toast, react query, and complete job form

--- reactjs_comprehensive/nextjs/projects/jobify/components/CreateJobForm.tsx
+++ reactjs_comprehensive/nextjs/projects/jobify/components/CreateJobForm.tsx
@@ -15,6 +15,11 @@ import { Form } from "@/components/ui/form";
 
 import { CustomFormField, CustomFormSelect } from "./FormComponents";
 
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createJobAction } from "@/utils/actions";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+
 function CreateJobForm() {
 	const form = useForm<CreateAndEditJobType>({
 		resolver: zodResolver(createAndEditJobSchema),
@@ -27,8 +32,27 @@ function CreateJobForm() {
 		},
 	});
 
+	const queryClient = useQueryClient();
+	const { toast } = useToast();
+	const router = useRouter();
+	const { mutate, isPending } = useMutation({
+		mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+		onSuccess: (data) => {
+			if (!data) {
+				toast({ description: "There was an error." });
+				return;
+			}
+			toast({ description: "Job created!" });
+			queryClient.invalidateQueries({ queryKey: ["jobs"] });
+			queryClient.invalidateQueries({ queryKey: ["stats"] });
+			queryClient.invalidateQueries({ queryKey: ["charts"] });
+      // form.reset();
+			router.push("/jobs");
+		},
+	});
+
 	function onSubmit(values: CreateAndEditJobType) {
-		console.log(values);
+		mutate(values);
 	}
 
 	return (
@@ -60,8 +84,12 @@ function CreateJobForm() {
 						labelText="job mode"
 						items={Object.values(JobMode)}
 					/>
-					<Button type="submit" className="self-end capitalize">
-						Create Job
+					<Button
+						type="submit"
+						className="self-end capitalize"
+						disabled={isPending}
+					>
+						{isPending ? "Loading" : "Create Job"}
 					</Button>
 				</div>
 			</form>
