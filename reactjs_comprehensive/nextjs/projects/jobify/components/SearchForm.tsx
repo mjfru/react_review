4b8e8ac1a33ff7e405d60aa4f188fcd036a15b79
Matt Frutchey <mjfru201@gmail.com>
2025-12-15 06:53:37 -0500

    begin building out the Search Job component

--- reactjs_comprehensive/nextjs/projects/jobify/components/SearchForm.tsx
+++ reactjs_comprehensive/nextjs/projects/jobify/components/SearchForm.tsx
@@ -1,5 +1,48 @@
+"use client";
+import { Input } from "./ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Button } from "./ui/button";
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { JobStatus } from "@/utils/types";
+
 function SearchForm() {
-	return <h1 className="text-4xl">SearchForm</h1>;
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = new FormData(e.currentTarget);
+		const search = formData.get("search") as string;
+		const jobStatus = formData.get("jobStatus") as string;
+		console.log(search, jobStatus);
+	};
+	return (
+		<form
+			className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
+			onSubmit={handleSubmit}
+		>
+			<Input type="text" placeholder="Search Jobs" name="search" />
+			<Select name="jobStatus">
+				<SelectTrigger>
+					<SelectValue />
+				</SelectTrigger>
+				<SelectContent>
+					{["all", ...Object.values(JobStatus)].map((jobStatus) => {
+						return (
+							<SelectItem key={jobStatus} value={jobStatus}>
+								{jobStatus}
+							</SelectItem>
+						);
+					})}
+				</SelectContent>
+			</Select>
+			<Button type="submit">Search</Button>
+		</form>
+	);
 }
 
 export default SearchForm;
