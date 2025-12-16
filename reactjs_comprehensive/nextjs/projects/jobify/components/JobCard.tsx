3504af62174bf244a7b4c415b4edc5ee72b54969
Matt Frutchey <mjfru201@gmail.com>
2025-12-16 10:23:12 -0500

    begin building out job results from the search form

--- reactjs_comprehensive/nextjs/projects/jobify/components/JobCard.tsx
+++ reactjs_comprehensive/nextjs/projects/jobify/components/JobCard.tsx
@@ -1,7 +1,41 @@
 import { JobType } from "@/utils/types";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 
-function JobCard({job}: {job: JobType}) {
-	return <h1 className="text-4xl">JobCard</h1>;
+import Link from "next/link";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
+import DeleteJobButton from "./DeleteJobButton";
+
+function JobCard({ job }: { job: JobType }) {
+	return (
+		<Card className="bg-muted">
+			<CardHeader>
+				<CardTitle>{job.position}</CardTitle>
+				<CardDescription>{job.company}</CardDescription>
+			</CardHeader>
+      <Separator />
+      <CardContent>
+
+      </CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/jobs/${job.id}`}>
+            Edit
+          </Link>
+        </Button>
+      </CardFooter>
+		</Card>
+	);
 }
 
 export default JobCard;
--- reactjs_comprehensive/nextjs/projects/jobify/components/SearchForm.tsx
+++ reactjs_comprehensive/nextjs/projects/jobify/components/SearchForm.tsx
@@ -13,20 +13,36 @@ import {
 import { JobStatus } from "@/utils/types";
 
 function SearchForm() {
+	const searchParams = useSearchParams();
+	const search = searchParams.get("search") || "";
+	const jobStatus = searchParams.get("jobStatus") || "all";
+
+	const router = useRouter();
+	const pathname = usePathname();
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget);
 		const search = formData.get("search") as string;
 		const jobStatus = formData.get("jobStatus") as string;
-		console.log(search, jobStatus);
+		// console.log(search, jobStatus);
+		let params = new URLSearchParams();
+		params.set("search", search);
+		params.set("jobStatus", jobStatus);
+		router.push(`${pathname}?${params.toString()}`);
 	};
 	return (
 		<form
 			className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
 			onSubmit={handleSubmit}
 		>
-			<Input type="text" placeholder="Search Jobs" name="search" />
-			<Select name="jobStatus">
+			<Input
+				type="text"
+				placeholder="Search Jobs"
+				name="search"
+				defaultValue={search}
+			/>
+			<Select name="jobStatus" defaultValue={jobStatus}>
 				<SelectTrigger>
 					<SelectValue />
 				</SelectTrigger>
