8c3f420b12354e0b101094008ebeef8f83452f09
Matt Frutchey <mjfru201@gmail.com>
2026-02-15 13:06:00 -0500

    finish player editing functionality

--- react_redux_nextjs/react_projects/02_deep_dive_game/src/components/Player.jsx
+++ react_redux_nextjs/react_projects/02_deep_dive_game/src/components/Player.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
 	const [isEditing, setIsEditing] = useState(false);
+	const [playerName, setPlayerName] = useState(initialName);
 
 	const handleEditClick = () => {
-		setIsEditing(!isEditing);
+		setIsEditing((editing) => !editing);
+	};
+
+	const handleChange = (e) => {
+		setPlayerName(e.target.value);
 	};
 
 	return (
 		<li>
 			<span className="player">
 				{isEditing ? (
-					<input type="text" required value={name} />
+					<input
+						type="text"
+						value={playerName}
+						required
+						onChange={handleChange}
+					/>
 				) : (
-					<span className="player-name">{name}</span>
+					<span className="player-name">{playerName}</span>
 				)}
 
 				<span className="player-symbol">{symbol}</span>
