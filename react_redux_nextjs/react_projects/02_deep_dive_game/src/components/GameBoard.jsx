b738300de2cee98f20357de8352cec8b4da64454
Matt Frutchey <mjfru201@gmail.com>
2026-02-16 12:59:19 -0500

    handle user clicks, make game board interactive

--- react_redux_nextjs/react_projects/02_deep_dive_game/src/components/GameBoard.jsx
+++ react_redux_nextjs/react_projects/02_deep_dive_game/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
@@ -5,14 +7,28 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	function handleSelectSquare(rowIndex, colIndex) {
+		setGameBoard((prevGameBoard) => {
+			const updatedBoard = [
+				...prevGameBoard.map(innerArray => [...innerArray]),
+			];
+			updatedBoard[rowIndex][colIndex] = "X";
+			return updatedBoard;
+		});
+	}
+
 	return (
 		<ol id="game-board">
-			{initialGameBoard.map((row, index) => (
-				<li key={index}>
+			{gameBoard.map((row, rowIndex) => (
+				<li key={rowIndex}>
 					<ol>
-						{row.map((playerSymbol, index) => (
-							<li key={index}>
-								<button>{playerSymbol}</button>
+						{row.map((playerSymbol, colIndex) => (
+							<li key={colIndex}>
+								<button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+									{playerSymbol}
+								</button>
 							</li>
 						))}
 					</ol>
