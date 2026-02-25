8ba8abe7844d129be41e1df990a9074680592a83
Matt Frutchey <mjfru201@gmail.com>
2026-02-25 10:48:47 -0500

    rematch functionality added, name display changed

--- react_redux_nextjs/react_projects/02_deep_dive_game/src/App.jsx
+++ react_redux_nextjs/react_projects/02_deep_dive_game/src/App.jsx
@@ -21,10 +21,14 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
 	const [gameTurns, setGameTurns] = useState([]);
+	const [players, setPlayers] = useState({
+		X: "Player 1",
+		O: "Player 2",
+	});
 
 	const activePlayer = deriveActivePlayer(gameTurns);
 
-	let gameBoard = initialGameBoard;
+	let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
 	// Deriving State:
 	for (const turn of gameTurns) {
@@ -48,7 +52,7 @@ function App() {
 			firstSquareSymbol === secondSquareSymbol &&
 			firstSquareSymbol === thirdSquareSymbol
 		) {
-			winner = firstSquareSymbol;
+			winner = players[firstSquareSymbol];
 		}
 	}
 
@@ -67,6 +71,19 @@ function App() {
 		});
 	}
 
+	function handleRestart() {
+		setGameTurns([]);
+	}
+
+	function handlePlayerNameChange(symbol, newName) {
+		setPlayers((prevPlayers) => {
+			return {
+				...prevPlayers,
+				[symbol]: [newName],
+			};
+		});
+	}
+
 	return (
 		<main>
 			<div id="game-container">
@@ -75,14 +92,18 @@ function App() {
 						initialName="Player 1"
 						symbol="X"
 						isActive={activePlayer === "X"}
+						onChangeName={handlePlayerNameChange}
 					/>
 					<Player
 						initialName="Player 2"
 						symbol="O"
 						isActive={activePlayer === "O"}
+						onChangeName={handlePlayerNameChange}
 					/>
 				</ol>
-				{(winner || hasDraw) && <GameOver winner={winner} />}
+				{(winner || hasDraw) && (
+					<GameOver winner={winner} onRestart={handleRestart} />
+				)}
 				<GameBoard
 					onSelectSquare={handleSelectSquare}
 					turns={gameTurns}
