fe8fdb45e78ff466f6b9720e1d56a8209787dd72
Matt Frutchey <mjfru201@gmail.com>
2026-04-03 07:48:49 -0400

    add and complete debugging sandbox exercise

---
+++ react_redux_nextjs/sandboxes/16debugging.jsx
@@ -0,0 +1,20 @@
+/*
+
+TODO For this exercise, imagine that colleagues gave you code they wrote for a React app they're working on.
+At the moment, this code is failing and it's your job to find and fix all errors that are hiding in the code.
+*/
+
+// don't change the Component name "App"
+export default function App() {
+    const clickHandler = () => {
+        console.log('Clicked!');
+    };
+    
+    return (
+        <div>
+          <h2>You're logged in!</h2>
+          <p>Welcome to your user profile!</p>
+          <button onClick={clickHandler}>Click me!</button>
+        </div>
+    );
+}
